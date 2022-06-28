86ef13f9a9f322670dc9c86c0c4bffe5f36543fb
不想睡觉的橘子君 <gengzhihao10@163.com>
2022-06-28 20:48:42 +0800

    0628 工厂类和bean

--- source code study/src/main/java/com/scs/Test.java
+++ source code study/src/main/java/com/scs/Test.java
@@ -1,6 +1,12 @@
 package com.scs;
 
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.EnumSet;
 import java.util.HashMap;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Test {
@@ -8,5 +14,11 @@ public class Test {
     public static void main(String[] args) {
         ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
         HashMap hashMap = new HashMap();
+//        Set<String> set = EnumSet.of("1,2,3");
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = RuntimeException.class, isolation = Isolation.DEFAULT)
+    public void testTransactional(){
+
     }
 }
