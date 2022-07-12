44c1ea7ddb1e7408f574703e8e9cb94f5cf56e69
GengZhihao <gengzh@asiainfo.com>
2022-07-12 14:34:34 +0800

    启动类

---
+++ middleware/src/main/java/com/mw/MiddlewareApplication.java
@@ -0,0 +1,15 @@
+package com.mw;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class MiddlewareApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(MiddlewareApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+}
