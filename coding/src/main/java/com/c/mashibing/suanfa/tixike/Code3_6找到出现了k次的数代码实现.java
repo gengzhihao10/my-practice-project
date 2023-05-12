79fb18303d939b08ad1ce2dc090c281fa0ae3825
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-05-12 14:27:09 +0800

    补充

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code3_6找到出现了k次的数代码实现.java
@@ -0,0 +1,13 @@
+package com.c.mashibing.suanfa.tixike;
+
+
+/**
+ * @author gengzhihao
+ * @date 2023/5/12 12:24
+ * @description
+ * 题目1，一个数组中有一种数出现了K次，其他数都出现了M次，M>1, K<M，
+ * 找到出现了K次的数，要求，额外空间复杂度O(1)，时间复杂度O(N)
+**/
+
+public class Code3_6找到出现了k次的数代码实现 {
+}
