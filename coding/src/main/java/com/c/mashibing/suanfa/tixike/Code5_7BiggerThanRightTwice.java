57091b511b8a871ab4c5d244a23dce232b8822fe
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-05-28 23:00:29 +0800

    题目

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code5_7BiggerThanRightTwice.java
@@ -0,0 +1,9 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，有一个数组，统计数组元素中，在元素右侧*2还小于元素的个数
+ 例如，[2,7,1,4,8,3]，对于7来说，符合要求的有1，3.
+ */
+public class Code5_7BiggerThanRightTwice {
+}
---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code6_1归并排序序.java
@@ -0,0 +1,9 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，给定一个数组arr，两个整数lower和upper，
+ 返回arr中有多少个子数组的累加和在[lower, upper]的范围上
+ */
+public class Code6_1归并排序序 {
+}
