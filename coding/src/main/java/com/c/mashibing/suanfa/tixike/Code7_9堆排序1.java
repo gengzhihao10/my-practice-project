4f856c75feef7b6d1880370954fd3155447fc27b
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-06-22 11:25:46 +0800

    最大线段重合问题，几乎有序数组排序问题

--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code7_9堆排序1.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code7_9堆排序1.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 /*
-todo
  前提，以数组为实际内存形式的完全二叉树，任意一个下标为i位置，其左右孩子下标为2*i+1, 2*i+2，父节点为i-1/2
  题目1，有一个现成的数组，对其进行堆排序。最主要的是要实现对大根堆向上调整heapinsert和向下调整heapify两个操作
  题目2，实现一个堆
@@ -31,57 +30,55 @@ public class Code7_9堆排序1 {
         }
 
         //1,将数组构造为大根堆
-        for(int i = arr.length-1; i >= 0; i--){
-            heapify(arr,i,arr.length);
+        for (int i = arr.length - 1; i >= 0; i--) {
+            heapify(arr, i, arr.length);
         }
 
         //2,调整成有序数组
         int heapSize = arr.length;
-        for (int i = arr.length; i > 0; i--){
-            swap(arr[0],arr[i-1]);
-            heapify(arr,0,--heapSize);
+        for (int i = arr.length; i > 0; i--) {
+            swap(arr[0], arr[i - 1]);
+            heapify(arr, 0, --heapSize);
         }
     }
 
     private static void heapInsert(int[] arr, int cur) {
         while (cur != 0) {
-            int parent = (cur-1) / 2;
+            int parent = (cur - 1) / 2;
             if (arr[parent] < arr[cur]) {
                 swap(arr[parent], arr[cur]);
                 cur = parent;
-            }
-            else {
+            } else {
                 break;
             }
         }
     }
 
-    public static void heapify(int[] arr, int cur,int heapSize){
-        while (cur < heapSize){
+    public static void heapify(int[] arr, int cur, int heapSize) {
+        while (cur < heapSize) {
             //左孩子大于最大下标，右孩子也会大于最大下标，此时不存在子节点
-            if (2 * cur + 1 >= heapSize){
+            if (2 * cur + 1 >= heapSize) {
                 break;
             }
             //到了这里，左孩子一定在数组中没越界，
-            int biggerChild = selectBiggerChild(arr,cur,heapSize);
-            if (arr[cur] < arr[biggerChild]){
-                swap(arr[cur],arr[biggerChild]);
+            int biggerChild = selectBiggerChild(arr, cur, heapSize);
+            if (arr[cur] < arr[biggerChild]) {
+                swap(arr[cur], arr[biggerChild]);
                 cur = biggerChild;
-            }
-            else {
+            } else {
                 break;
             }
         }
     }
 
     //返回较大的孩子节点
-    private static int selectBiggerChild(int[] arr, int cur, int heapSize){
+    private static int selectBiggerChild(int[] arr, int cur, int heapSize) {
         // 如果右孩子越界，那么只有左孩子应该成为较大的孩子；
-        if (2 * cur + 2 >= heapSize){
+        if (2 * cur + 2 >= heapSize) {
             return 2 * cur + 1;
         }
         //如果右孩子没越界，那么在左右孩子中选一个较大的子节点对应的下标返回
-        if (arr[2*cur+1] > arr[2*cur+2]){
+        if (arr[2 * cur + 1] > arr[2 * cur + 2]) {
             return 2 * cur + 1;
         }
         return 2 * cur + 2;
@@ -151,15 +148,15 @@ public class Code7_9堆排序1 {
         return arr;
     }
 
-    public static void bubbleSort(int[] arr){
-        if (arr == null || arr.length < 2){
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
             return;
         }
 
-        for (int i = arr.length-1; i > 0; i--){
-            for (int j =0; j < i; j++){
-                if (arr[j] < arr[j+1]){
-                    swap(arr[j],arr[j+1]);
+        for (int i = arr.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    swap(arr[j], arr[j + 1]);
                 }
             }
 
@@ -183,86 +180,84 @@ public class Code7_9堆排序1 {
         private List<T> list = new ArrayList<>();
         private int heapSize = 0;
 
-        public boolean isEmpty(){
+        public boolean isEmpty() {
             return heapSize == 0;
         }
 
-        public int size(){
+        public int size() {
             return heapSize;
         }
 
         //因为没有反向索引表，所以复杂度做不到Logn，只能做到n
-        public boolean contains(T element){
-            for (int i = 0; i < heapSize; i++){
-                if (list.get(i) == element ){
+        public boolean contains(T element) {
+            for (int i = 0; i < heapSize; i++) {
+                if (list.get(i) == element) {
                     return true;
                 }
             }
             return false;
         }
 
-        public T peek(){
+        public T peek() {
             return list.get(0);
         }
 
-        public T poll(){
+        public T poll() {
             T result = list.get(0);
-            swap(list,0,--heapSize);
+            swap(list, 0, --heapSize);
             list.remove(heapSize);
-            heapify(list,0,heapSize);
+            heapify(list, 0, heapSize);
             return result;
         }
 
-        public void offer(T element){
+        public void offer(T element) {
             list.add(element);
-            heapInsert(list,heapSize++,comparator);
+            heapInsert(list, heapSize++, comparator);
         }
 
         private void swap(List<T> list, int i, int j) {
             T temp = list.get(i);
-            list.set(i,list.get(j));
-            list.set(j,temp);
+            list.set(i, list.get(j));
+            list.set(j, temp);
         }
 
 
-        public void heapify(List<T> list, int cur,int heapSize){
-            while (cur < heapSize){
+        public void heapify(List<T> list, int cur, int heapSize) {
+            while (cur < heapSize) {
                 //左孩子大于最大下标，右孩子也会大于最大下标，此时不存在子节点
-                if (2 * cur + 1 >= heapSize){
+                if (2 * cur + 1 >= heapSize) {
                     break;
                 }
                 //到了这里，左孩子一定在数组中没越界，
-                int child = selectChild(list,cur,heapSize,comparator);
-                if (comparator.compare(list.get(cur),list.get(child)) > 0 ){
-                    swap(list,cur,child);
+                int child = selectChild(list, cur, heapSize, comparator);
+                if (comparator.compare(list.get(cur), list.get(child)) > 0) {
+                    swap(list, cur, child);
                     cur = child;
-                }
-                else {
+                } else {
                     break;
                 }
             }
         }
 
-        private int selectChild(List<T> list, int cur, int heapSize,Comparator<T> comparator){
+        private int selectChild(List<T> list, int cur, int heapSize, Comparator<T> comparator) {
             // 如果右孩子越界，那么只有左孩子应该成为较大的孩子；
-            if (2 * cur + 2 >= heapSize){
+            if (2 * cur + 2 >= heapSize) {
                 return 2 * cur + 1;
             }
             //小于0表示要让左边参数放在前面
-            if (comparator.compare(list.get(2*cur+1), list.get(2*cur+2)) < 0){
+            if (comparator.compare(list.get(2 * cur + 1), list.get(2 * cur + 2)) < 0) {
                 return 2 * cur + 1;
             }
             return 2 * cur + 2;
         }
 
-        private void heapInsert(List<T> list, int cur,Comparator<T> comparator) {
+        private void heapInsert(List<T> list, int cur, Comparator<T> comparator) {
             while (cur != 0) {
-                int parent = (cur-1) / 2;
+                int parent = (cur - 1) / 2;
                 if (comparator.compare(list.get(parent), list.get(cur)) > 0) {
-                    swap(list,parent, cur);
+                    swap(list, parent, cur);
                     cur = parent;
-                }
-                else {
+                } else {
                     break;
                 }
             }
@@ -270,7 +265,7 @@ public class Code7_9堆排序1 {
 
     }
 
-    public static class Student{
+    public static class Student {
         int age;
         String name;
 
@@ -285,33 +280,105 @@ public class Code7_9堆排序1 {
         }
     }
 
-    //题目2的测试
-    public static void main(String[] args) {
-        MyPriorityQueue<Student> heap = new MyPriorityQueue<>(new Comparator<Student>() {
-            @Override
-            public int compare(Student o1, Student o2) {
-                return o1.age - o2.age;
+//    //题目2的测试
+//    public static void main(String[] args) {
+//        MyPriorityQueue<Student> heap = new MyPriorityQueue<>(new Comparator<Student>() {
+//            @Override
+//            public int compare(Student o1, Student o2) {
+//                return o1.age - o2.age;
+//            }
+//        });
+//        Student s1 = new Student(11, "张三");
+//        Student s2 = new Student(14, "李四");
+//        Student s3 = new Student(39, "贾斯汀.比伯");
+//        Student s4 = new Student(7, "汤姆");
+//        Student s5 = new Student(45, "李建国");
+//        Student s6 = new Student(23, "罗辑");
+//        Student s7 = new Student(19, "程心");
+//        Student s8 = new Student(34, "章北海");
+//
+//        heap.offer(s1);
+//        heap.offer(s2);
+//        heap.offer(s3);
+//        heap.offer(s4);
+//        heap.offer(s5);
+//        System.out.println(heap.peek());
+//        System.out.println(heap.poll());
+//        ;
+//        System.out.println(heap.poll());
+//        ;
+//        System.out.println(heap.poll());
+//        ;
+//        System.out.println(heap.poll());
+//        ;
+//
+//    }
+
+
+    //***************************************************************************
+    /*
+     * @author gengzhihao
+     * @date 2023/6/22 8:38
+     * @description 题目3
+     * @param arr
+     * @param k
+     **/
+    public static void qs3_process1(int[] arr, int k) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        if (k == 0 ){
+            return;
+        }
+
+        int start = 0;
+        for (int i = 0; i < k+1; i++){
+            heapInsert(arr,i,0);
+        }
+
+        for (int i = k+1; i < arr.length; i++){
+            start++;
+            heapInsert(arr,i,start);
+        }
+
+    }
+
+    private static void heapInsert(int[] arr, int cur, int start) {
+        while (cur >= start) {
+            int parent = (cur - 1) / 2;
+            if (arr[parent] < arr[cur]) {
+                swap(arr[parent], arr[cur]);
+                cur = parent;
+            } else {
+                break;
             }
-        });
-        Student s1 = new Student(11,"张三");
-        Student s2 = new Student(14,"李四");
-        Student s3 = new Student(39,"贾斯汀.比伯");
-        Student s4 = new Student(7,"汤姆");
-        Student s5 = new Student(45,"李建国");
-        Student s6 = new Student(23,"罗辑");
-        Student s7 = new Student(19,"程心");
-        Student s8 = new Student(34,"章北海");
+        }
+    }
 
-        heap.offer(s1);
-        heap.offer(s2);
-        heap.offer(s3);
-        heap.offer(s4);
-        heap.offer(s5);
-        System.out.println(heap.peek());
-        System.out.println(heap.poll());;
-        System.out.println(heap.poll());;
-        System.out.println(heap.poll());;
-        System.out.println(heap.poll());;
 
+        public static void main(String[] args) {
+        int testTime = 100000;
+        int maxLength = 100;
+        int maxValue = 1000;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = randomArray(maxLength, maxValue);
+//            int[] arr1 = {6,2,7};
+            int[] arr2 = copyArray(arr1);
+            int[] temp = copyArray(arr1);
+//            LogarithmicUtil.printArray(temp);
+            int k = (int)(Math.random()*arr1.length/2)+1;
+//            System.out.println("k为："+k);
+            qs3_process1(arr1,k);
+            bubbleSort(arr2);
+            if (!equalOrNot(arr1, arr2)) {
+                System.out.println("错误啦");
+                System.out.print("原数组：");
+                LogarithmicUtil.printArray(temp);
+                System.out.print("错误排序后：");
+                LogarithmicUtil.printArray(arr1);
+                System.out.println();
+            }
+        }
     }
 }
--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code8_2最大线段重合问题用堆实现.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code8_2最大线段重合问题用堆实现.java
@@ -1,5 +1,9 @@
 package com.c.mashibing.suanfa.tixike;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /*
 todo
  题目1，给定很多线段，每个线段都有两个数组[start, end]
@@ -10,4 +14,138 @@ todo
  返回线段最多重合区域中，包含了几条线段
  */
 public class Code8_2最大线段重合问题用堆实现 {
+
+    //线段
+    public static class Line{
+        int start;
+        int end;
+
+        public Line(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    public static class StartComparator implements Comparator<Line> {
+
+        @Override
+        public int compare(Line o1, Line o2) {
+            return o1.start - o2.start;
+        }
+    }
+
+    /*
+     * @author gengzhihao
+     * @date 2023/6/22 9:22
+     * @description 题目1
+     * @param arr
+     * @return int
+     **/
+    public static int qs1_process1(int[][] arr){
+        if (arr == null || arr.length < 2){
+            return 0;
+        }
+        //1,将数组通过排序变得有序(根据线段的start排序)
+        Line[] lines = new Line[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            lines[i] = new Line(arr[i][0], arr[i][1]);
+        }
+        Arrays.sort(lines, new StartComparator());
+
+        //2,记录最大值
+        int max = 0;
+        //heap默认是小根堆
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for (Line line : lines) {
+            //2.1 将heap中小于start的数都弹出来
+            while (!heap.isEmpty() && heap.peek() < line.start) {
+                heap.poll();
+            }
+            //2.2 heap中剩余的end都是大于当前线段start的end，保存数量
+            max = Math.max(max, heap.size());
+            //2.3 将当前线段的end加入到heap中
+            heap.offer(line.end);
+        }
+        return max+1;
+    }
+
+
+
+    //题目1 对数器
+    public static void main(String[] args) {
+        int testTime = 10000;
+        int maxValue = 10000;
+        int maxLength = 10000;
+
+//        for (int i = 0; i < testTime ; i++){
+//            int[][] arr1 = generateArray(maxValue,maxLength);
+            int[][] arr1 = {{1,9},{0,7},{8,9}};
+            int[][] arr2 = copyarr(arr1);
+            int[][] arr3 = copyarr(arr1);
+
+            int max1 = qs1_process1(arr1);
+            int max2 = getMax(arr2);
+            if (max1 != max2){
+                System.out.println("出错了");
+                printArray(arr3);
+                System.out.println("max1为 "+max1);
+                System.out.println("max2为 "+max2);
+            }
+//        }
+    }
+
+    private static void printArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++){
+            System.out.print("[" + arr[i][0] + " " + arr[i][1] + "], ");
+        }
+        System.out.println();
+    }
+
+    //题目1的题解
+    private static int getMax(int[][] lines) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < lines.length; i++) {
+            min = Math.min(min, lines[i][0]);
+            max = Math.max(max, lines[i][1]);
+        }
+        int cover = 0;
+        for (double p = min + 0.5; p < max; p += 1) {
+            int cur = 0;
+            for (int i = 0; i < lines.length; i++) {
+                if (lines[i][0] < p && lines[i][1] > p) {
+                    cur++;
+                }
+            }
+            cover = Math.max(cover, cur);
+        }
+        return cover;
+    }
+
+    //复制二维数组
+    private static int[][] copyarr(int[][] arr) {
+        int[][] result = new int[arr.length][2];
+
+        for (int i = 0; i < arr.length; i++){
+            result[i][0] = arr[i][0];
+            result[i][1] = arr[i][1];
+        }
+        return result;
+    }
+
+    //产生的数组是二维数组
+    private static int[][] generateArray(int maxValue, int maxLength) {
+        int lentgh = (int)(Math.random() * maxLength)+3;
+        int v1,v2;
+        int[][] arr = new int[lentgh][2];
+        for (int i = 0; i < lentgh; i++){
+            do {
+                v1 = (int)(Math.random() * maxValue);
+                v2 = (int)(Math.random() * maxValue);
+            }while (v1 >= v2);
+            arr[i][0] = v1;
+            arr[i][1] = v2;
+        }
+        return arr;
+    }
 }
