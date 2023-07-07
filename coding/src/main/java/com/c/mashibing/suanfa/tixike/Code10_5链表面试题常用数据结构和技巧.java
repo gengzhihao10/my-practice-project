f07667dde50b74cf63dacb1f24bb78eae4c17dd3
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-07-07 14:09:07 +0800

    链表面试题

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code10_5链表面试题常用数据结构和技巧.java
@@ -0,0 +1,11 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1， 判断输入的链表是否为回文结构。可以使用额外空间
+ 题目2，判断输入的链表是否为回文结构。要求只用有限几个变量
+ 题目3，一个链表，假设其节点从左到右为L1 L2 L3 L4 R1 R2 R3 R4，
+ 将其转变为L1 R4 L2 R3 L3 R2 L4 R1
+ */
+public class Code10_5链表面试题常用数据结构和技巧 {
+}
---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code10_6常见的面试题解析.java
@@ -0,0 +1,10 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，输入一个链表的头节点和一个参数V，将链表调整为：
+ 小于V的节点->等于V的节点->大于V的节点。可以使用额外空间
+ 题目2，题目1，但是要使用有限几个变量
+ */
+public class Code10_6常见的面试题解析 {
+}
