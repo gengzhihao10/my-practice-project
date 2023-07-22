8f953f7cbeb094beddef265d787a6d96e8e4908d
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-07-22 11:16:57 +0800

    题目实现：多叉树变二叉树二叉树变多叉树，求二叉树最大宽度

--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code12_6EncodeNAryTreeToBinaryTree1.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code12_6EncodeNAryTreeToBinaryTree1.java
@@ -1,40 +1,169 @@
 package com.c.mashibing.suanfa.tixike;
 
+import org.springframework.util.CollectionUtils;
+
+import java.util.LinkedList;
 import java.util.List;
 
 /*
-todo
  题目1，有一颗多叉树，想要按照二叉树的序列化方式得到的结果，再通过二叉树的反序列化的方式能够得到原有的多叉树
  */
 public class Code12_6EncodeNAryTreeToBinaryTree1 {
 
     //多叉树节点
     private static class Node{
-        int value;
+        char value;
         List<Node> nexts;
 
         public Node() {
         }
 
-        public Node(int value) {
+        public Node(char value) {
             this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+    }
+
+    //二叉树节点
+    private static class TreeNode {
+        char value;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode() {
+        }
+
+        public TreeNode(char value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
         }
     }
 
     /*
      * @author gengzhihao
      * @date 2023/7/20 14:16
-     * @description 题目1，序列化
+     * @description 题目1，
      * @param root
      * @return String
      **/
-    public static String qs1_serialize(Node root){
+    public static TreeNode qs1_serialize(Node root){
+        if (root == null){
+            return null;
+        }
+        TreeNode newRoot = serialize(root);
+        return newRoot;
+    }
+
+    private static TreeNode serialize(Node root) {
+        if (root == null){
+            return null;
+        }
+        TreeNode head = new TreeNode(root.value);
+        TreeNode cur = null;
+        if (CollectionUtils.isEmpty(root.nexts)){
+            return head;
+        }
+        for (Node node : root.nexts){
+            if (cur == null){
+                cur = serialize(node);
+                head.left = cur;
+                continue;
+            }else {
+                cur.right = serialize(node);
+            }
+            cur = cur.right;
+        }
+        return head;
+    }
+
+    /*
+     * @author gengzhihao
+     * @date 2023/7/22 10:13
+     * @description 题目1，二叉树变多叉树
+     * @param root
+     * @return Node
+     **/
+    public static Node qs1_deserialize(TreeNode root){
         if (root == null){
             return null;
         }
 
-
+        return desirialize(root);
     }
 
 
+    private static Node desirialize(TreeNode root) {
+        if (root == null){
+            return null;
+        }
+        List<Node> children = new LinkedList<>();
+        Node head = new Node(root.value);
+        if (root.left == null){
+            return head;
+        }
+        children.add(desirialize(root.left));
+        TreeNode cur = root.left.right;
+        while (cur != null){
+            children.add(desirialize(cur));
+            cur = cur.right;
+        }
+        head.nexts = children;
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node('a');
+        Node b = new Node('b');
+        Node c = new Node('c');
+        Node d = new Node('d');
+        Node e = new Node('e');
+        Node f = new Node('f');
+        Node g = new Node('g');
+        Node h = new Node('h');
+        Node i = new Node('i');
+        Node j = new Node('j');
+        Node k = new Node('k');
+        Node l = new Node('l');
+        Node m = new Node('m');
+        Node n = new Node('n');
+        a.nexts = new LinkedList<>();
+        a.nexts.add(b);
+        a.nexts.add(c);
+        a.nexts.add(d);
+        a.nexts.add(e);
+
+        b.nexts = new LinkedList<>();
+        b.nexts.add(f);
+
+        c.nexts = new LinkedList<>();
+        c.nexts.add(g);
+        c.nexts.add(h);
+
+        d.nexts = new LinkedList<>();
+        d.nexts.add(i);
+        d.nexts.add(j);
+        d.nexts.add(k);
+        d.nexts.add(l);
+
+        e.nexts = new LinkedList<>();
+        e.nexts.add(m);
+        e.nexts.add(n);
+
+        //b下面的f没有序列化出来
+        TreeNode newRoot = qs1_serialize(a);
+
+         System.out.println("test");
+
+         Node newRoot2 = qs1_deserialize(newRoot);
+        System.out.println("test2");
+    }
 }
--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code12_8打印一颗二叉树求树的最大宽度.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code12_8打印一颗二叉树求树的最大宽度.java
@@ -1,9 +1,90 @@
 package com.c.mashibing.suanfa.tixike;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /*
-todo
  题目1，设计一个能够打印二叉树的打印函数(很少考，不写了)
  题目2，秋二叉树最宽的层有多少个节点
  */
 public class Code12_8打印一颗二叉树求树的最大宽度 {
+
+    //二叉树
+    private static class Node{
+        int value;
+        Node left;
+        Node right;
+
+        public Node() {
+        }
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+    }
+
+    /*
+     * @author gengzhihao
+     * @date 2023/7/22 11:04
+     * @description 题目2
+     * @param root
+     * @return int
+     **/
+    public static int qs2_process1(Node root){
+        if (root == null){
+            return 0;
+        }
+
+        int maxWidth = 0;
+        int width = 0;
+        Node curEnd = root,nextEnd = null;
+        Queue<Node> queue = new LinkedList<>();
+        Node cur = root;
+        queue.offer(cur);
+
+        while (!queue.isEmpty()){
+            cur = queue.poll();
+            if (cur.left != null){
+                queue.offer(cur.left);
+                nextEnd = cur.left;
+            }
+            if (cur.right != null){
+                queue.offer(cur.right);
+                nextEnd = cur.right;
+            }
+            width++;
+            if (cur == curEnd){
+                maxWidth = Math.max(maxWidth,width);
+                curEnd = nextEnd;
+                width = 0;
+            }
+        }
+        return maxWidth;
+    }
+
+
+    public static void main(String[] args) {
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        Node n4 = new Node(4);
+        Node n5 = new Node(5);
+        Node n6 = new Node(6);
+        Node n7 = new Node(7);
+        Node n8 = new Node(8);
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n3.left = n6;
+        n3.right = n7;
+        n6.left = n8;
+
+        System.out.println(qs2_process1(n1));
+    }
 }
