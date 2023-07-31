9db9487fa98b4f2bccb0d5a37c0af86ab8ea0d6d
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-07-31 10:40:48 +0800

    新题目：排队的最大快乐值

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code14_2返回a和b的最低公共祖先.java
@@ -0,0 +1,10 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，给定一颗二叉树的头节点head，和另外两个节点a和b，
+ 返回a和b的最低公共祖先，可以使用更多的额外空间
+ 题目2，类似题目1，使用递归套路解题
+ */
+public class Code14_2返回a和b的最低公共祖先 {
+}
---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code14_3多叉树代表公司结构.java
@@ -0,0 +1,15 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，派对的最大快乐值，
+ 现有一个派对要邀请公司员工，邀请了一个员工，那么他的直接上下级就不能来了，要求排队的最大快乐值
+    员工信息的定义如下:
+    class Employee {
+        public int happy; // 这名员工可以带来的快乐值
+        List<Employee> nexts; // 这名员工有哪些直接下级
+    }
+
+ */
+public class Code14_3多叉树代表公司结构 {
+}
