b160ac2f48b56c1738eb7f11163f9715088375fd
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-08-07 20:20:22 +0800

    新题目：实现并查集

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code15_19并查集.java
@@ -0,0 +1,12 @@
+package com.c.mashibing.suanfa.tixike;
+
+
+/*
+todo
+ 题目1，实现一个并查集
+ UnionSet(List<V> list);//构造方法
+ void union(V a, V b);
+ boolean isSameSet(V a, V b);
+ */
+public class Code15_19并查集 {
+}
