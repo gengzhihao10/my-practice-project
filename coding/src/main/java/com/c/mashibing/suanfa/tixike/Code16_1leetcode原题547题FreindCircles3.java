5006b2e8499ddf47aab945f8c652f50e5534c98a
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-08-11 14:57:42 +0800

    新题目：岛问题，随机输入岛位置

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code16_1leetcode原题547题FreindCircles3.java
@@ -0,0 +1,25 @@
+package com.c.mashibing.suanfa.tixike;
+
+import java.util.List;
+
+/*
+todo
+ 题目1，https://leetcode.com/problems/number-of-islands-ii/
+ 岛问题，但是岛的位置是一个个位置随机输入的，返回每一步的连通区域的数量
+ 题目2，上一题，但是m*n远大于输入的k个位置
+ */
+public class Code16_1leetcode原题547题FreindCircles3 {
+
+    /*
+     * @author gengzhihao
+     * @date 2023/8/11 9:41
+     * @description 题目1
+     * @param m
+     * @param n
+     * @param positions
+     * @return List<Integer>
+     **/
+    public static List<Integer> qs1_process1(int m, int n, int[][] positions){
+
+    }
+}
---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code16_1leetcode原题547题FreindCircles4.java
@@ -0,0 +1,9 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，岛问题（扩展）
+ 如果matrix极大，设计一种可行的并行计算方案
+ */
+public class Code16_1leetcode原题547题FreindCircles4 {
+}
