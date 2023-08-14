e0fde93f6cf60b467a05fd7079211542cc7e0cd6
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-08-14 14:47:54 +0800

    新题目：图的深度优先遍历和宽度优先遍历

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code17_6宽度优先遍历.java
@@ -0,0 +1,18 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，实现图的宽度优先遍历
+ */
+public class Code17_6宽度优先遍历 {
+
+    public static void bfs(Node start){
+
+    }
+
+    private static class Graph{}
+
+    private static class Edge{}
+
+    private static class Node{}
+}
---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code17_7深度优先遍历.java
@@ -0,0 +1,19 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，实现图的深度优先遍历
+ */
+public class Code17_7深度优先遍历 {
+
+    public static void dfs(Node node){
+
+    }
+
+
+    private static class Graph{}
+
+    private static class Edge{}
+
+    private static class Node{}
+}
