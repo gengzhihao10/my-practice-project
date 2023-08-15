5e036d3282467397cd857d1d52ae0c0d0cd91023
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-08-15 10:24:55 +0800

    题目实现：图的深度优先遍历和宽度优先遍历

--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code17_6宽度优先遍历.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code17_6宽度优先遍历.java
@@ -1,18 +1,93 @@
 package com.c.mashibing.suanfa.tixike;
 
+import java.util.*;
+
 /*
-todo
  题目1，实现图的宽度优先遍历
  */
 public class Code17_6宽度优先遍历 {
 
+    /*
+     * @author gengzhihao
+     * @date 2023/8/15 8:45
+     * @description 题目1
+     * @param start
+     **/
     public static void bfs(Node start){
+        if (start == null){
+            return;
+        }
+
+        Set<Node> set = new HashSet<>();
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(start);
+        set.add(start);
+        Node cur;
+        List<Node> nexts;
+        while (!queue.isEmpty()){
+            cur = queue.poll();
+            nexts = cur.nexts;
+            System.out.println("  " + cur.value);
+            for (Node node : nexts){
+                if (!set.contains(node)){
+                    queue.offer(node);
+                    set.add(node);
+                }
+            }
+        }
 
     }
 
-    private static class Graph{}
+    //图
+    private static class Graph{
 
-    private static class Edge{}
+        HashMap<Integer,Node> nodes;
+        HashSet<Edge> edges;
 
-    private static class Node{}
+        public Graph() {
+        }
+
+        public Graph(HashMap<Integer, Node> nodes, HashSet<Edge> edges) {
+            this.nodes = nodes;
+            this.edges = edges;
+        }
+    }
+
+    //边
+    private static class Edge{
+        int weight;
+        Node start;
+        Node end;
+
+        public Edge() {
+        }
+
+        public Edge(int weight, Node start, Node end) {
+            this.weight = weight;
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    //点
+    private static class Node{
+        //这个点的值
+        int value;
+        //入度，有几条边进来
+        int in;
+        //出度，有几条边出去
+        int out;
+        //从这个点出发，下面的点有几个
+        ArrayList<Node> nexts;
+        //从这个点出发，下面的边有几个
+        ArrayList<Edge> edges;
+
+        public Node(int value) {
+            this.value = value;
+            in = 0;
+            out = 0;
+            nexts = new ArrayList<>();
+            edges = new ArrayList<>();
+        }
+    }
 }
--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code17_7深度优先遍历.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code17_7深度优先遍历.java
@@ -1,19 +1,92 @@
 package com.c.mashibing.suanfa.tixike;
 
+import java.util.*;
+
 /*
 todo
  题目1，实现图的深度优先遍历
  */
 public class Code17_7深度优先遍历 {
 
+    /*
+     * @author gengzhihao
+     * @date 2023/8/15 9:31
+     * @description 题目1
+     * @param node
+     **/
     public static void dfs(Node node){
+        if (node == null){
+            return;
+        }
 
+        Stack<Node> stack = new Stack<>();
+        Set<Node> set = new HashSet<>();
+        stack.push(node);
+        System.out.println("  " + node.value);
+        set.add(node);
+
+        Node cur;
+        List<Node> nexts;
+        while (!stack.isEmpty()){
+            cur = stack.pop();
+            nexts = cur.nexts;
+            for (Node next : nexts){
+                if (!set.contains(next)){
+                    stack.push(cur);
+                    stack.push(next);
+                    set.add(next);
+                    System.out.println("  " + next.value);
+                    break;
+                }
+            }
+        }
     }
 
 
-    private static class Graph{}
+    private static class Graph{
+        HashMap<Integer,Node> nodes;
+        HashSet<Edge> edges;
 
-    private static class Edge{}
+        public Graph() {
+        }
 
-    private static class Node{}
+        public Graph(HashMap<Integer, Node> nodes, HashSet<Edge> edges) {
+            this.nodes = nodes;
+            this.edges = edges;
+        }
+    }
+
+    private static class Edge{
+        Node start;
+        Node end;
+        int weight;
+
+        public Edge() {
+        }
+
+        public Edge(Node start, Node end, int weight) {
+            this.start = start;
+            this.end = end;
+            this.weight = weight;
+        }
+    }
+
+    private static class Node{
+        int value;
+        int in;
+        int out;
+        ArrayList<Node> nexts;
+        ArrayList<Edge> edges;
+
+        public Node() {
+        }
+
+        public Node(int value) {
+            this.value = value;
+            in = 0;
+            out = 0;
+            nexts = new ArrayList<>();
+            edges = new ArrayList<>();
+        }
+    }
 }
