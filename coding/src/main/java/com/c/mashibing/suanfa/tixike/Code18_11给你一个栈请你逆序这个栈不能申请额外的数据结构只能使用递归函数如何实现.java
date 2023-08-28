074002daba09388c116084c1274917d15f222a05
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-08-28 11:54:28 +0800

    新题目：机器人走路问题

--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code18_11给你一个栈请你逆序这个栈不能申请额外的数据结构只能使用递归函数如何实现.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code18_11给你一个栈请你逆序这个栈不能申请额外的数据结构只能使用递归函数如何实现.java
@@ -1,9 +1,22 @@
 package com.c.mashibing.suanfa.tixike;
 
+import java.util.Stack;
+
 /*
 todo
  题目1，给你一个栈，请你逆序这个栈，
- 不能申请额外的数据结构，只能使用地柜函数，如何实现
+ 不能申请额外的数据结构，只能使用递归函数，如何实现
  */
 public class Code18_11给你一个栈请你逆序这个栈不能申请额外的数据结构只能使用递归函数如何实现 {
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/8/28 10:11
+     * @description 题目1
+     * @param stack
+     **/
+    public static void qs1_process1(Stack stack){
+
+    }
 }
---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code19_2假设有拍成一行的N个位置.java
@@ -0,0 +1,31 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 假设有排成一行的N个位置，记为1~N，N 一定大于或等于 2
+ 开始时机器人在其中的M位置上(M 一定是 1~N 中的一个)
+ 如果机器人来到1位置，那么下一步只能往右来到2位置；
+ 如果机器人来到N位置，那么下一步只能往左来到 N-1 位置；
+ 如果机器人来到中间位置，那么下一步可以往左走或者往右走；
+ 规定机器人必须走 K 步，最终能来到P位置(P也是1~N中的一个)的方法有多少种
+ 给定四个参数 N、M、K、P，返回方法数。
+
+ */
+public class Code19_2假设有拍成一行的N个位置 {
+
+    /*
+     * @author gengzhihao
+     * @date 2023/8/28 11:28
+     * @description 题目1
+     * @param N 有从1到N的座位
+     * @param start 机器人的起始位置
+     * @param aim 机器人的目标位置
+     * @param K 机器人能走几步
+     * @return int 机器人从start走到aim，一共走K步，能有几条路径
+     **/
+    public static int qs1_process1(int N, int start, int aim, int K) {
+
+
+        return 0;
+    }
+}
