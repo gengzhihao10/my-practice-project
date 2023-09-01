394c81904dd822eb6a295014786e105794699f86
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-09-01 14:55:03 +0800

    新题目：背包问题、字符串转换方案问题

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code20_1背包问题.java
@@ -0,0 +1,24 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，
+ 给定两个长度都为N的数组weights和values，
+ weights[i]和values[i]分别代表 i号物品的重量和价值。
+ 给定一个正数bag，表示一个载重bag的袋子，
+ 你装的物品不能超过这个重量。
+ 返回你能装下最多的价值是多少?
+ 暴力递归方法实现
+ 题目2，使用动态规划方法实现
+ */
+public class Code20_1背包问题 {
+
+    // 所有的货，重量和价值，都在w和v数组里
+    // 为了方便，其中没有负数
+    // bag背包容量，不能超过这个载重
+    // 返回：不超重的情况下，能够得到的最大价值
+    public static int maxValue(int[] w, int[] v, int bag){
+
+        return 0;
+    }
+}
---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code20_5规定1对应A2对应B3对应C依次类推.java
@@ -0,0 +1,20 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，
+ 规定1和A对应、2和B对应、3和C对应...26和Z对应
+ 那么一个数字字符串比如"111”就可以转化为:
+ "AAA"、"KA"和"AK"
+ 给定一个只有数字字符组成的字符串str，返回有多少种转化结果
+ 使用暴力递归方法实现
+ 题目2，使用动态规划实现
+ */
+public class Code20_5规定1对应A2对应B3对应C依次类推 {
+
+
+
+    public static int number(String str){
+        return 0;
+    }
+}
