e65abaf28d19fe7a3c495ec355436076782fbe15
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-09-02 11:08:38 +0800

    题目实现：背包问题、字符串转换方案问题

--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code20_1背包问题.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code20_1背包问题.java
@@ -1,7 +1,6 @@
 package com.c.mashibing.suanfa.tixike;
 
 /*
-todo
  题目1，
  给定两个长度都为N的数组weights和values，
  weights[i]和values[i]分别代表 i号物品的重量和价值。
@@ -17,8 +16,69 @@ public class Code20_1背包问题 {
     // 为了方便，其中没有负数
     // bag背包容量，不能超过这个载重
     // 返回：不超重的情况下，能够得到的最大价值
-    public static int maxValue(int[] w, int[] v, int bag){
-
-        return 0;
+    public static int qs1_process1(int[] w, int[] v, int bag){
+        if (w == null || w.length == 0 || v == null || v.length != w.length || bag < 0){
+            return -1;
+        }
+        return process1(w,v,0,bag);
     }
+
+    //返回：从index处开始，到结束时，背包剩余容量rest能装的下的最大价值
+    private static int process1(int[] weight, int[] value, int index, int rest) {
+        if (rest < 0){
+            return -1;
+        }
+        if (index == weight.length){
+            return 0;
+        }
+
+        int pos1 = process1(weight,value,index+1,rest);
+        if (rest-weight[index] < 0){
+            return pos1;
+        }
+        int pos2 = value[index] + process1(weight,value,index+1,rest-weight[index]);
+        return Math.max(pos1,pos2);
+    }
+
+    //****************************************************************************************
+
+    /*
+     * @author gengzhihao
+     * @date 2023/9/2 10:27
+     * @description 题目2
+     * @param w
+     * @param v
+     * @param bag
+     * @return int
+     **/
+    public static int qs2_process1(int[] w, int[] v, int bag){
+        if (w == null || w.length == 0 || v == null || v.length != w.length || bag < 0){
+            return -1;
+        }
+        int N = w.length + 1;
+        int M = bag+1;
+        int[][] ans = new int[N][M];
+        for (int index = N-2; index >= 0; index--){
+            for (int rest = 0; rest < M; rest++){
+                int pos1 = ans[index+1][rest];
+                int pos2 = 0;
+                if (rest-w[index] >= 0){
+                    pos2 = v[index] + ans[index+1][rest-w[index]];
+                }
+                ans[index][rest] = Math.max(pos1,pos2);
+            }
+        }
+        return ans[0][bag];
+    }
+
+    public static void main(String[] args) {
+        int[] weights = { 3, 2, 4, 7, 3, 1, 7 };
+        int[] values = { 5, 6, 3, 19, 12, 4, 2 };
+        int bag = 15;
+        int res1 = qs1_process1(weights,values,bag);
+        System.out.println(res1);
+        int res2 = qs2_process1(weights,values,bag);
+        System.out.println(res2);
+    }
+
 }
--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code20_5规定1对应A2对应B3对应C依次类推.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code20_5规定1对应A2对应B3对应C依次类推.java
@@ -1,7 +1,6 @@
 package com.c.mashibing.suanfa.tixike;
 
 /*
-todo
  题目1，
  规定1和A对应、2和B对应、3和C对应...26和Z对应
  那么一个数字字符串比如"111”就可以转化为:
@@ -14,7 +13,125 @@ public class Code20_5规定1对应A2对应B3对应C依次类推 {
 
 
 
-    public static int number(String str){
-        return 0;
+    /*
+     * @author gengzhihao
+     * @date 2023/9/2 10:38
+     * @description 题目1
+     * @param str
+     * @return int
+     **/
+    public static int qs1_process1(String str){
+        if (str == null || str.length() == 0){
+            return 0;
+        }
+        return process1(str.toCharArray(),0);
+    }
+
+    //返回：str从index位置开始，到右边结束，能够找到的最多的转化可能性
+    private static int process1(char[] chars, int index) {
+        if (index >= chars.length){
+            return 1;
+        }
+        //返回0，走不下去的情况
+        if (chars[index] == '0'){
+            return 0;
+        }
+
+        int res = 0;
+        //单个的算，从1到9
+        res += process1(chars,index+1);
+        //2个字符转一个数，从10到26
+        if (index+1 < chars.length &&  (chars[index] - '0')*10 + (chars[index+1] - '0') <= 26){
+            res += process1(chars,index+2);
+        }
+        return res;
+    }
+
+    //*****************************************************************************************
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/9/2 11:00
+     * @description 题目2
+     * @param str
+     * @return int
+     **/
+    public static int qs2_process1(String str){
+        if (str == null || str.length() == 0){
+            return 0;
+        }
+        char[] chars = str.toCharArray();
+        int[] ans = new int[chars.length+1];
+        ans[chars.length] = 1;
+
+        for (int index = chars.length-1; index >= 0; index--){
+            if (chars[index] == '0'){
+                ans[index] = 0;
+                continue;
+            }
+            int res = 0;
+            res += ans[index+1];
+            if (index+1 < chars.length &&  (chars[index] - '0')*10 + (chars[index+1] - '0') <= 26){
+                res += ans[index+2];
+            }
+            ans[index] = res;
+        }
+        return ans[0];
+    }
+
+
+
+    //*********************************************************************************************
+
+    public static void main(String[] args) {
+        int N = 30;
+        int testTime = 1000000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int len = (int) (Math.random() * N);
+            String s = randomString(len);
+            int ans0 = qs1_process1(s);
+            int ans1 = qs2_process1(s);
+            if (ans0 != ans1) {
+                System.out.println(s);
+                System.out.println(ans0);
+                System.out.println(ans1);
+                System.out.println("Oops!");
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+
+
+    // 为了测试
+    public static String randomString(int len) {
+        char[] str = new char[len];
+        for (int i = 0; i < len; i++) {
+            str[i] = (char) ((int) (Math.random() * 10) + '0');
+        }
+        return String.valueOf(str);
+    }
+
+
+    public static int dp1(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        char[] str = s.toCharArray();
+        int N = str.length;
+        int[] dp = new int[N + 1];
+        dp[N] = 1;
+        for (int i = N - 1; i >= 0; i--) {
+            if (str[i] != '0') {
+                int ways = dp[i + 1];
+                if (i + 1 < str.length && (str[i] - '0') * 10 + str[i + 1] - '0' < 27) {
+                    ways += dp[i + 2];
+                }
+                dp[i] = ways;
+            }
+        }
+        return dp[0];
     }
 }
