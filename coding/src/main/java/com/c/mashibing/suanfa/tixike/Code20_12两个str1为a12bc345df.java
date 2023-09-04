28ddc504ead55c9817ee91e2e45d6f02a2aee481
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-09-04 15:03:18 +0800

    新题目：字符串最长公共子序列长度、贴纸问题

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code20_12两个str1为a12bc345df.java
@@ -0,0 +1,25 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，
+ 给定两个字符串str1和str2，
+ 返回这两个字符串的最长公共子序列长度
+ 比如 ： str1 = “a12b3c456d”,str2 = “1ef23ghi4j56k”
+ 最长公共子序列是“123456”，所以返回长度6
+ 暴力递归
+ 题目2，同题目1，表直接计算的动态规划
+ */
+public class Code20_12两个str1为a12bc345df {
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/9/4 11:21
+     * @description 题目1
+     * @param s1
+     * @param s2
+     * @return int
+     **/
+    public static int longestCommonSubsequence1(String s1, String s2){}
+}
---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code20_9贴纸abcbbacck要贴出aabbcc的样式1.java
@@ -0,0 +1,50 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，
+ 给定一个字符串str，给定一个字符串类型的数组arr，出现的字符都是小写英文
+ arr每一个字符串，代表一张贴纸，你可以把单个字符剪开使用，目的是拼出str来
+ 返回需要至少多少张贴纸可以完成这个任务。
+ 例子：str= "babac"，arr = {"ba","c","abcd"}
+ ba + ba + c  3  abcd + abcd 2  abcd+ba 2
+ 所以返回2
+ 暴力递归
+ 题目2，同题目1，优化递归
+ 题目3，同题目2，加缓存
+ */
+public class Code20_9贴纸abcbbacck要贴出aabbcc的样式1 {
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/9/4 11:12
+     * @description 题目1
+     * @param stickers
+     * @param target
+     * @return int
+     **/
+    public static int minStickers1(String[] stickers, String target){}
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/9/4 11:13
+     * @description 题目2
+     * @param stickers
+     * @param target
+     * @return int
+     **/
+    public static int minStickers2(String[] stickers, String target){}
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/9/4 11:13
+     * @description 题目3
+     * @param stickers
+     * @param target
+     * @return int
+     **/
+    public static int minStickers2(String[] stickers, String target){}
+}
