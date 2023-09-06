49798ea843f0c38d627bf397d490db40dad2081b
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-09-06 11:24:05 +0800

    新题目：最长回文子序列

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code21_1返回最长回文子序列长度.java
@@ -0,0 +1,25 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，
+ 给定一个字符串str，返回这个字符串的最长回文子序列长度
+ 比如 ： str = “a12b3c43def2ghi1kpm”
+ 最长回文子序列是“1234321”或者“123c321”，返回长度7
+ 暴力递归
+ 题目2，基于严格位置依赖的动态规划
+ 题目3，基于记忆化搜素的动态规划，小优化，优化的常数时间
+ */
+public class Code21_1返回最长回文子序列长度 {
+
+    /*
+     * @author gengzhihao
+     * @date 2023/9/6 10:22
+     * @description 题目1
+     * @param s
+     * @return int
+     **/
+    public static int lpsl1(String s){
+        return 0;
+    }
+}
