ab5f6e8bcc818855966ef369ea6ccc3386ca4d83
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-09-07 11:33:19 +0800

    新题目：象棋马跳跃到指定位置有几条路径问题

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code21_6返回象棋从一个位置到另一个位置的方法有多少种.java
@@ -0,0 +1,34 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，
+ 请同学们自行搜索或者想象一个象棋的棋盘，
+ 然后把整个棋盘放入第一象限，棋盘的最左下角是(0,0)位置
+ 那么整个棋盘就是横坐标上9条线、纵坐标上10条线的区域
+ 给你三个 参数 x，y，k
+ 返回“马”从(0,0)位置出发，必须走k步
+ 最后落在(x,y)上的方法数有多少种?
+ 暴力递归
+ 题目2，基于严格位置依赖的动态规划
+ */
+public class Code21_6返回象棋从一个位置到另一个位置的方法有多少种 {
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/9/7 11:14
+     * @description 题目1
+     * @param a
+     * @param b
+     * @param k
+     * @return int
+     **/
+    public static int jump(int a, int b, int k){
+
+
+        return 0;
+    }
+
+
+}
