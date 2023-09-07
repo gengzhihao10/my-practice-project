d15b313ceecd19b8b83fc34fa3b6439ab5cba2d2
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-09-07 10:17:19 +0800

    题目实现：最长回文子序列

--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code21_1返回最长回文子序列长度.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code21_1返回最长回文子序列长度.java
@@ -1,7 +1,6 @@
 package com.c.mashibing.suanfa.tixike;
 
 /*
-todo
  题目1，
  给定一个字符串str，返回这个字符串的最长回文子序列长度
  比如 ： str = “a12b3c43def2ghi1kpm”
@@ -20,6 +19,107 @@ public class Code21_1返回最长回文子序列长度 {
      * @return int
      **/
     public static int lpsl1(String s){
-        return 0;
+        if (s == null || s.length() == 0){
+            return 0;
+        }
+
+        return process1(s.toCharArray(),0,s.length()-1);
+    }
+
+    //返回从L到R下标对应的字符中最长的回文子序列的长度
+    private static int process1(char[] chars, int L, int R) {
+        if (L == R){
+            return 1;
+        }
+        if (L == R-1){
+            return chars[L] == chars[R] ? 2 : 1;
+        }
+
+        //下面3种，是基于L和R位置一定不一样的情况下的3种可能性
+        int p1 = process1(chars,L+1,R-1);
+        int p2 = process1(chars,L,R-1);
+        int p3 = process1(chars,L+1,R);
+        //下面的1种，是基于L和R位置如果一样的可能性
+        int p4 = chars[L] == chars[R] ? 2 + process1(chars,L+1,R-1) : 0;
+
+        return Math.max(Math.max(p1,p2),Math.max(p3,p4));
+    }
+
+    /*
+     * @author gengzhihao
+     * @date 2023/9/7 9:53
+     * @description 题目2
+     * @param s
+     * @return int
+     **/
+    public static int lpsl2(String s){
+        if (s == null || s.length() == 0){
+            return 0;
+        }
+
+        //1 申请空白表
+        int N = s.length();
+        int[][] ans = new int[N][N];
+        char[] chars = s.toCharArray();
+
+        //2 初始化空白表
+        ans[N-1][N-1] = 1;
+        for (int L = 0; L < N-1; L++){
+            ans[L][L] = 1;
+            ans[L][L+1] = chars[L] == chars[L+1] ? 2 : 1;
+        }
+
+        //3 计算
+        for (int L = N-2; L > 0; L--){
+            for (int R = L + 2; R < N; R++){
+                int p1 = ans[L+1][R-1];
+                int p2 = ans[L][R-1];
+                int p3 = ans[L+1][R];
+                int p4 = chars[L] == chars[R] ? 2 + ans[L+1][R-1] : 0;
+                ans[L][R] = Math.max(Math.max(p1,p2),Math.max(p3,p4));
+            }
+        }
+
+        //4 返回结果
+        return ans[0][N-1];
+    }
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/9/7 10:12
+     * @description 题目3
+     * @param s
+     * @return int
+     **/
+    public static int lpsl3(String s){
+        if (s == null || s.length() == 0){
+            return 0;
+        }
+
+        //1 申请空白表
+        int N = s.length();
+        int[][] ans = new int[N][N];
+        char[] chars = s.toCharArray();
+
+        //2 初始化空白表
+        ans[N-1][N-1] = 1;
+        for (int L = 0; L < N-1; L++){
+            ans[L][L] = 1;
+            ans[L][L+1] = chars[L] == chars[L+1] ? 2 : 1;
+        }
+
+        //3 计算
+        for (int L = N-2; L > 0; L--){
+            for (int R = L + 2; R < N; R++){
+                int p2 = ans[L][R-1];
+                int p3 = ans[L+1][R];
+                int p4 = chars[L] == chars[R] ? 2 + ans[L+1][R-1] : 0;
+                ans[L][R] = Math.max(p2,Math.max(p3,p4));
+            }
+        }
+
+        //4 返回结果
+        return ans[0][N-1];
     }
 }
