bcf6ca703442d414be0bc623a7893009c08b2408
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-09-13 14:54:27 +0800

    新题目：货币面值数组问题、Bob生存概率问题

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code22_6返回组成aim的方法数arr是面值货币数组.java
@@ -0,0 +1,30 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，
+ arr是货币数组，其中的值都是正数。再给定一个正数aim。
+ 每个值都认为是一张货币，
+ 认为值相同的货币没有任何不同，
+ 返回组成aim的方法数
+ 例如：arr = {1,2,1,1,2,1,2}，aim = 4
+ 方法：1+1+1+1、1+1+2、2+2
+ 一共就3种方法，所以返回3
+ 动态规划
+ 题目2，同题目1，基于动态转移的优化
+ */
+public class Code22_6返回组成aim的方法数arr是面值货币数组 {
+
+    /*
+     * @author gengzhihao
+     * @date 2023/9/13 14:44
+     * @description 暴力递归
+     * @param arr
+     * @param aim
+     * @return int
+     **/
+    public static int coinsWay0(int[] arr, int aim){
+        return 0;
+    }
+
+}
---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code22_7返k步之后Bob还在NM区域的概率.java
@@ -0,0 +1,30 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，
+ 给定5个参数，N，M，row，col，k
+ 表示在N*M的区域上，醉汉Bob初始在(row,col)位置
+ Bob一共要迈出k步，且每步都会等概率向上下左右四个方向走一个单位
+ 任何时候Bob只要离开N*M的区域，就直接死亡
+ 返回k步之后，Bob还在N*M的区域的概率
+
+ */
+public class Code22_7返k步之后Bob还在NM区域的概率 {
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/9/13 14:42
+     * @description 暴力递归
+     * @param row Bob所在行
+     * @param col Bob所在列
+     * @param k 有k步可以走
+     * @param N 生存区域有N行
+     * @param M 生存区域有M列
+     * @return double Bob k步走完的生存概率
+     **/
+    public static double livePosibility0(int row, int col, int k, int N, int M){
+        return 0;
+    }
+}
