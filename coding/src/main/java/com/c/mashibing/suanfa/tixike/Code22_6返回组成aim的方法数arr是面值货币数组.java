a33c2d89d97d93fdfb22b86f1091dea5015b25e8
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-09-14 11:57:21 +0800

    题目实现：货币面值数组问题、Bob生存概率问题

--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code22_6返回组成aim的方法数arr是面值货币数组.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code22_6返回组成aim的方法数arr是面值货币数组.java
@@ -1,7 +1,9 @@
 package com.c.mashibing.suanfa.tixike;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
-todo
  题目1，
  arr是货币数组，其中的值都是正数。再给定一个正数aim。
  每个值都认为是一张货币，
@@ -24,7 +26,228 @@ public class Code22_6返回组成aim的方法数arr是面值货币数组 {
      * @return int
      **/
     public static int coinsWay0(int[] arr, int aim){
-        return 0;
+        if (arr == null || arr.length == 0){
+            return  0;
+        }
+
+        Info info = getInfo(arr);
+        return process0(info.coins,info.zhangs,0,aim);
     }
 
+    /*
+     * @author gengzhihao
+     * @date 2023/9/14 9:12
+     * @description 从index位置出发向后推进，到结束时，有多少可能性，正好凑齐rest
+     * @param valueArr 数值代表面值，该数组表示面值有几种
+     * @param sizeArr 数值表示在valueArr中对应索引位置的面值有几张
+     * @param index 表示valueArr和sizeArr都从index位置向后推进
+     * @param rest 需要凑齐的目标
+     * @return int 有几种可能性
+     **/
+    private static int process0(int[] valueArr, int[] sizeArr, int index, int rest) {
+        if (index == valueArr.length){
+            return rest == 0 ? 1 : 0;
+        }
+
+        int sum = 0;
+        for (int zhang = 0; (zhang <= sizeArr[index]) && (rest-zhang * valueArr[index] >= 0); zhang++){
+            sum += process0(valueArr,sizeArr,index+1,rest-zhang * valueArr[index]);
+        }
+        return sum;
+    }
+
+    //统计arr的面值和对应的张数
+    private static Info getInfo(int[] arr) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i : arr){
+            if (!map.containsKey(i)){
+                map.put(i,1);
+            }else {
+                map.put(i,map.get(i)+1);
+            }
+        }
+        int[] value = new int[map.size()];
+        int[] size = new int[map.size()];
+        int i = 0;
+        for (Map.Entry<Integer,Integer> entry: map.entrySet()){
+            value[i] = entry.getKey();
+            size[i++] = entry.getValue();
+        }
+        return new Info(value,size);
+    }
+
+    //对面值的统计信息
+    private static class Info{
+        int[] coins;//面值大小
+        int[] zhangs;//有几张该面值
+
+        public Info() {
+        }
+
+        public Info(int[] coins, int[] zhangs) {
+            this.coins = coins;
+            this.zhangs = zhangs;
+        }
+    }
+
+    //*************************************************************************************************
+
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/9/14 9:57
+     * @description 题目1
+     * @param arr
+     * @param aim
+     * @return int
+     **/
+    public static int coinsWay1(int[] arr, int aim){
+        if (arr == null || arr.length == 0){
+            return  0;
+        }
+
+        Info info = getInfo(arr);
+        int[] valueArr = info.coins;
+        int[] sizeArr = info.zhangs;
+
+        //申请空白dp表
+        int N = valueArr.length;
+        int[][] dp = new int[N+1][aim+1];
+        //index==N这一行初始化
+        dp[N][0] = 1;
+
+        for (int index = N-1; index >= 0; index--){
+            for (int rest = aim; rest >= 0; rest--){
+                int sum = 0;
+                for (int zhang = 0; (zhang <= sizeArr[index]) && (rest-zhang * valueArr[index] >= 0); zhang++){
+                    sum += dp[index+1][rest-zhang * valueArr[index]];
+                }
+                dp[index][rest] = sum;
+            }
+        }
+        return dp[0][aim];
+    }
+
+
+    //**********************************************************************************************
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/9/14 10:08
+     * @description 题目2
+     * @param arr
+     * @param aim
+     * @return int
+     **/
+    public static int coinsWay2(int[] arr, int aim){
+        if (arr == null || arr.length == 0){
+            return  0;
+        }
+
+        Info info = getInfo(arr);
+        int[] coins = info.coins;
+        int[] zhangs = info.zhangs;
+
+        //申请空白dp表
+        int N = coins.length;
+        int[][] dp = new int[N+1][aim+1];
+        //index==N这一行初始化
+        dp[N][0] = 1;
+
+        for (int index = N-1; index >= 0; index--){
+            for (int rest = 0; rest <= aim; rest++){
+                dp[index][rest] = dp[index + 1][rest];
+                //最大张数+1才是(index,rest)点下方左侧最远点的纵坐标
+                if ((rest - (zhangs[index] + 1) * coins[index]) >= 0){
+                    dp[index][rest] -= dp[index+1][rest - (zhangs[index] + 1) * coins[index]];
+                }
+                if ((rest - coins[index] >= 0)){
+                    dp[index][rest] += dp[index][rest - coins[index]];
+                }
+            }
+        }
+//        print(dp);
+        return dp[0][aim];
+    }
+
+    //************************************************************************************************
+
+    // 为了测试
+    public static int[] randomArray(int maxLen, int maxValue) {
+        int N = (int) (Math.random() * maxLen);
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = (int) (Math.random() * maxValue) + 1;
+        }
+        return arr;
+    }
+
+    // 为了测试
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // 为了测试
+    public static void main(String[] args) {
+        int maxLen = 10;
+        int maxValue = 20;
+        int testTime = 1000000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = randomArray(maxLen, maxValue);
+            int aim = (int) (Math.random() * maxValue);
+            int ans1 = ans(arr, aim);
+            int ans2 = coinsWay2(arr, aim);
+//            int ans3 = dp2(arr, aim);
+            if (ans1 != ans2 ) {
+                System.out.println("Oops!");
+                printArray(arr);
+                System.out.println(aim);
+                System.out.println(ans1);
+                System.out.println(ans2);
+//                System.out.println(ans3);
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+
+    public static int ans(int[] arr, int aim) {
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return 0;
+        }
+        Info info = getInfo(arr);
+        int[] coins = info.coins;
+        int[] zhangs = info.zhangs;
+        int N = coins.length;
+        int[][] dp = new int[N + 1][aim + 1];
+        dp[N][0] = 1;
+        for (int index = N - 1; index >= 0; index--) {
+            for (int rest = 0; rest <= aim; rest++) {
+                int ways = 0;
+                for (int zhang = 0; zhang * coins[index] <= rest && zhang <= zhangs[index]; zhang++) {
+                    ways += dp[index + 1][rest - (zhang * coins[index])];
+                }
+                dp[index][rest] = ways;
+            }
+        }
+//        print(dp);
+        return dp[0][aim];
+    }
+
+    private static void print(int[][] arr){
+        System.out.println("打印二维数组");
+        for (int i = 0; i < arr.length; i++){
+            for (int j = 0; j < arr[i].length; j++){
+                System.out.print("  " + arr[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("打印结束");
+    }
 }
--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code22_7返k步之后Bob还在NM区域的概率.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code22_7返k步之后Bob还在NM区域的概率.java
@@ -20,11 +20,108 @@ public class Code22_7返k步之后Bob还在NM区域的概率 {
      * @param row Bob所在行
      * @param col Bob所在列
      * @param k 有k步可以走
-     * @param N 生存区域有N行
-     * @param M 生存区域有M列
+     * @param N 生存区域为第0~N-1行
+     * @param M 生存区域为第0~M-1列
      * @return double Bob k步走完的生存概率
      **/
-    public static double livePosibility0(int row, int col, int k, int N, int M){
-        return 0;
+    public static double livePosibility0(int row, int col, int k, int N, int M) {
+        if (row < 0 || col < 0 || k < 0 || N < 0 || M < 0) {
+            return -1;
+        }
+        return process0(row, col, k, N, M) / Math.pow(4, k);
+    }
+
+
+    private static double process0(int row, int col, int k, int N, int M) {
+        //踏出生存区域，直接死亡，不可能生存
+        if (row < 0 || row == N || col < 0 || col == M) {
+            return 0;
+        }
+        //当前点在生存区域中，如果k为0，说明生存下来了
+        if (k == 0) {
+            return 1;
+        }
+
+        return process0(row + 1, col, k - 1, N, M)
+                + process0(row, col + 1, k - 1, N, M)
+                + process0(row - 1, col, k - 1, N, M)
+                + process0(row, col - 1, k - 1, N, M);
+    }
+
+    //*******************************************************************************************
+
+    /*
+     * @author gengzhihao
+     * @date 2023/9/14 11:37
+     * @description 题目2
+     * @param row
+     * @param col
+     * @param k
+     * @param N
+     * @param M
+     * @return double
+     **/
+    public static double livePosibility1(int row, int col, int k, int N, int M) {
+        if (row < 0 || col < 0 || k < 0 || N < 0 || M < 0) {
+            return -1;
+        }
+
+        //申请空白dp表
+        int[][][] dp = new int[N][M][k + 1];
+        //初始化dp表
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                dp[i][j][0] = 1;
+            }
+        }
+
+        //计算
+        for (int k1 = 1; k1 <= k; k1++) {
+            for (int row1 = 0; row1 < N; row1++) {
+                for (int col1 = 0; col1 < M; col1++) {
+                    dp[row1][col1][k1] = getVaue(dp,row1+1,col1,k1-1,N,M)
+                            + getVaue(dp,row1-1,col1,k1-1,N,M)
+                            + getVaue(dp,row1,col1+1,k1-1,N,M)
+                            + getVaue(dp,row1,col1-1,k1-1,N,M);
+                }
+            }
+        }
+        return dp[row][col][k] / Math.pow(4,k);
+    }
+
+    //将递归时跳出生存区域的情况，在这里包括进来
+    private static int getVaue(int[][][] dp,int row,int col,int k,int N,int M){
+        if (row < 0 || row == N || col < 0 || col == M) {
+            return 0;
+        }
+        return dp[row][col][k];
+    }
+
+    //*************************************************************************************
+
+    public static void main(String[] args) {
+        System.out.println(livePosibility1(6, 6, 10, 50, 50));
+        System.out.println(ans(6, 6, 10, 50, 50));
+    }
+
+    public static double ans(int row, int col, int k, int N, int M) {
+        return (double) process(row, col, k, N, M) / Math.pow(4, k);
+    }
+
+    // 目前在row，col位置，还有rest步要走，走完了如果还在棋盘中就获得1个生存点，返回总的生存点数
+    public static long process(int row, int col, int rest, int N, int M) {
+        if (row < 0 || row == N || col < 0 || col == M) {
+            return 0;
+        }
+        // 还在棋盘中！
+        if (rest == 0) {
+            return 1;
+        }
+        // 还在棋盘中！还有步数要走
+        long up = process(row - 1, col, rest - 1, N, M);
+        long down = process(row + 1, col, rest - 1, N, M);
+        long left = process(row, col - 1, rest - 1, N, M);
+        long right = process(row, col + 1, rest - 1, N, M);
+        return up + down + left + right;
     }
 }
