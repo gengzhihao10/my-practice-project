5539dfd4dd2e060bf9ce5413726c4da2fff2e280
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-09-16 15:58:04 +0800

    新题目：货币最小组成数量问题

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code23_6返回组成aim的最少货币数.java
@@ -0,0 +1,25 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，
+ arr是面值数组，其中的值都是正数且没有重复。再给定一个正数aim。
+ 每个值都认为是一种面值，且认为张数是无限的。
+ 返回组成aim的最少货币数
+ 动态规划
+ 题目2，同题目1，进行斜率优化
+ */
+public class Code23_6返回组成aim的最少货币数 {
+
+    /*
+     * @author gengzhihao
+     * @date 2023/9/16 15:57
+     * @description 暴力递归
+     * @param arr
+     * @param aim
+     * @return int
+     **/
+    public static int minCoins0(int[] arr, int aim){
+        return 0;
+    }
+}
