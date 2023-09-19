e40ced3998cc3a67a7c401c597cdbef9ace7f47a
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-09-19 14:18:37 +0800

    新题目：正数分裂问题、两个集合累加和接近问题

--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code23_6返回组成aim的最少货币数.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code23_6返回组成aim的最少货币数.java
@@ -1,7 +1,6 @@
 package com.c.mashibing.suanfa.tixike;
 
 /*
-todo
  题目1，
  arr是面值数组，其中的值都是正数且没有重复。再给定一个正数aim。
  每个值都认为是一种面值，且认为张数是无限的。
---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code23_9返回裂开的数的种类.java
@@ -0,0 +1,29 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，
+ 给定一个正数n，求n的裂开方法数，
+ 规定：后面的数不能比前面的数小
+ 比如4的裂开方法有：
+ 1+1+1+1、1+1+2、1+3、2+2、4
+ 5种，所以返回5
+
+ */
+public class Code23_9返回裂开的数的种类 {
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/9/19 9:51
+     * @description 暴力递归
+     * @param n
+     * @return int
+     **/
+    public static int ways0(int n){
+
+        return 0;
+    }
+
+
+}
