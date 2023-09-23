c096b8b717dd8fe3b7fee909392fd0ca30d9323d
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-09-23 14:26:48 +0800

    新题目：N皇后问题

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code24_15N皇后问题1.java
@@ -0,0 +1,27 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，
+ N皇后问题是指在N*N的棋盘上要摆N个皇后，
+ 要求任何两个皇后不同行、不同列， 也不在同一条斜线上
+ 给定一个整数n，返回n皇后的摆法有多少种。n=1，返回1
+ n=2或3，2皇后和3皇后问题无论怎么摆都不行，返回0
+ n=8，返回92
+ N皇后问题无法改成动态规划
+ 题目2，同题目1，使用位运算优化常数时间
+ */
+public class Code24_15N皇后问题1 {
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/9/23 9:29
+     * @description 暴力递归
+     * @param n
+     * @return int
+     **/
+    public static int num1(int n){
+        return 0;
+    }
+}
