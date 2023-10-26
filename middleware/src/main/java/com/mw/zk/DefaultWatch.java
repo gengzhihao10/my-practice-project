b7dfaf20e1411db4cc248ad51d61b8ef7707639c
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-10-26 22:09:31 +0800

    基于zookeeper的分布式锁的实现

---
+++ middleware/src/main/java/com/mw/zk/DefaultWatch.java
@@ -0,0 +1,36 @@
+package com.mw.zk;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+import java.util.concurrent.CountDownLatch;
+
+
+/**
+ * @author: 马士兵教育
+ * @create: 2019-09-20 13:53
+ */
+public class DefaultWatch implements Watcher {
+    CountDownLatch init = new CountDownLatch(1) ;
+
+    public CountDownLatch getInit() {
+        return init;
+    }
+
+
+    @Override
+    public void process(WatchedEvent event) {
+        Event.KeeperState state = event.getState();
+
+        switch (state) {
+            case Disconnected:
+                System.out.println("Disconnected...c...new...");
+                init =  new CountDownLatch(1);
+                break;
+            case SyncConnected:
+                System.out.println("Connected...c...ok...");
+                init.countDown();
+                break;
+        }
+    }
+}
---
+++ middleware/src/main/java/com/mw/zk/TestLock.java
@@ -0,0 +1,56 @@
+package com.mw.zk;
+
+import org.apache.zookeeper.ZooKeeper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestLock {
+
+
+    ZooKeeper zk;
+    DefaultWatch defaultWatch;
+
+    @Before
+    public void conn(){
+        String address = "192.168.184.129:2181,192.168.184.130:2181,192.168.184.131:2181,192.168.184.132:2181/testLock";
+        zk = ZKUtils.getZK(address,1000);
+    }
+
+    @After
+    public void close(){
+        ZKUtils.closeZK();
+    }
+
+    @Test
+    public void testlock(){
+        for (int i = 0; i < 10; i++) {
+            new Thread(){
+                @Override
+                public void run() {
+                    WatchCallBack watchCallBack = new WatchCallBack();
+                    watchCallBack.setZk(zk);
+                    String name = Thread.currentThread().getName();
+                    watchCallBack.setThreadName(name);
+
+                    try {
+                        //tryLock
+                        watchCallBack.tryLock();
+                        System.out.println(name + " at work");
+                        watchCallBack.getRootData();
+//                        Thread.sleep(1000);
+                        //unLock
+                        watchCallBack.unLock();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }.start();
+        }
+       while(true){
+
+       }
+    }
+}
