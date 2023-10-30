3113feb6efd3d5714c52c0a1acd7b2a022ff5421
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-10-30 14:29:07 +0800

    新题目：滑动窗口

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code25_2每一个滑出状态的最大值.java
@@ -0,0 +1,97 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，假设一个固定大小为W的窗口，依次划过arr，
+ 返回每一次滑出状况的最大值
+ 例如，arr=[4,3,5,4,3,3,6,7],W = 3
+ 返回[5,5,5,4,6,7]
+ */
+public class Code25_2每一个滑出状态的最大值 {
+
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/10/30 11:06
+     * @description 题目1
+     * @param arr
+     * @param w
+     * @return int[]
+     **/
+    public static int[] getMaxWindow(int[] arr, int w){
+        return null;
+    }
+
+
+    //*************************************************************************************************************
+    // 暴力的对数器方法
+    public static int[] right(int[] arr, int w) {
+        if (arr == null || w < 1 || arr.length < w) {
+            return null;
+        }
+        int N = arr.length;
+        int[] res = new int[N - w + 1];
+        int index = 0;
+        int L = 0;
+        int R = w - 1;
+        while (R < N) {
+            int max = arr[L];
+            for (int i = L + 1; i <= R; i++) {
+                max = Math.max(max, arr[i]);
+
+            }
+            res[index++] = max;
+            L++;
+            R++;
+        }
+        return res;
+    }
+
+
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * (maxValue + 1));
+        }
+        return arr;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int testTime = 100000;
+        int maxSize = 100;
+        int maxValue = 100;
+        System.out.println("test begin");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generateRandomArray(maxSize, maxValue);
+            int w = (int) (Math.random() * (arr.length + 1));
+            int[] ans1 = getMaxWindow(arr, w);
+            int[] ans2 = right(arr, w);
+            if (!isEqual(ans1, ans2)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("test finish");
+    }
+}
---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code25_3arr中达标子数组的数量.java
@@ -0,0 +1,11 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，给定一个整形数组arr,和一个整数num，
+ 某个arr中的子数组sub，如果想达标，必须满足：
+ sub中最大值-sub中最小值<=num
+ 返回arr中达标子数组的数量
+ */
+public class Code25_3arr中达标子数组的数量 {
+}
