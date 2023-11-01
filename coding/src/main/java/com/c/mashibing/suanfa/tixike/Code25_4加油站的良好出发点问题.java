0b39c9142f7ec5bb2fbeaa7a80c6933357601899
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-11-01 11:40:08 +0800

    题目实现：加油站的良好出发点问题

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code25_4加油站的良好出发点问题.java
@@ -0,0 +1,21 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，加油站的良好出发点问题
+ 测试链接：https://leetcode.com/problems/gas-station
+ */
+public class Code25_4加油站的良好出发点问题 {
+
+    /*
+     * @author gengzhihao
+     * @date 2023/11/1 10:59
+     * @description 题目1
+     * @param gas
+     * @param cost
+     * @return int
+     **/
+    public static int canCompleteCircuit(int[] gas, int[] cost){
+        return 0;
+    }
+}
