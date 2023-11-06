ebb561a8227a72ea47aa431fa15ca654f208da75
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-11-06 14:33:12 +0800

    新题目：单调栈

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code26_1单调栈结构介绍.java
@@ -0,0 +1,127 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，数组arr中没有重复值，实现一个单调栈
+ 题目2，数组arr中有重复值，实现一个单调栈
+ */
+public class Code26_1单调栈结构介绍 {
+
+    /*
+     * @author gengzhihao
+     * @date 2023/11/6 11:47
+     * @description 题目1
+     * @param arr
+     * @return int[][]
+     **/
+    public static int[][] getNearLessNoRepeat(int[] arr){
+        return null;
+    }
+
+    /*
+     * @author gengzhihao
+     * @date 2023/11/6 11:47
+     * @description 题目2
+     * @param arr
+     * @return int[][]
+     **/
+    public static int[][] getNearLess(int[] arr){
+        return null;
+    }
+
+    // for test
+    public static int[] getRandomArrayNoRepeat(int size) {
+        int[] arr = new int[(int) (Math.random() * size) + 1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            int swapIndex = (int) (Math.random() * arr.length);
+            int tmp = arr[swapIndex];
+            arr[swapIndex] = arr[i];
+            arr[i] = tmp;
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] getRandomArray(int size, int max) {
+        int[] arr = new int[(int) (Math.random() * size) + 1];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * max) - (int) (Math.random() * max);
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[][] rightWay(int[] arr) {
+        int[][] res = new int[arr.length][2];
+        for (int i = 0; i < arr.length; i++) {
+            int leftLessIndex = -1;
+            int rightLessIndex = -1;
+            int cur = i - 1;
+            while (cur >= 0) {
+                if (arr[cur] < arr[i]) {
+                    leftLessIndex = cur;
+                    break;
+                }
+                cur--;
+            }
+            cur = i + 1;
+            while (cur < arr.length) {
+                if (arr[cur] < arr[i]) {
+                    rightLessIndex = cur;
+                    break;
+                }
+                cur++;
+            }
+            res[i][0] = leftLessIndex;
+            res[i][1] = rightLessIndex;
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[][] res1, int[][] res2) {
+        if (res1.length != res2.length) {
+            return false;
+        }
+        for (int i = 0; i < res1.length; i++) {
+            if (res1[i][0] != res2[i][0] || res1[i][1] != res2[i][1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int size = 10;
+        int max = 20;
+        int testTimes = 2000000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTimes; i++) {
+            int[] arr1 = getRandomArrayNoRepeat(size);
+            int[] arr2 = getRandomArray(size, max);
+            if (!isEqual(getNearLessNoRepeat(arr1), rightWay(arr1))) {
+                System.out.println("Oops!");
+                printArray(arr1);
+                break;
+            }
+            if (!isEqual(getNearLess(arr2), rightWay(arr2))) {
+                System.out.println("Oops!");
+                printArray(arr2);
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+}
