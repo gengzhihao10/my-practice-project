a35ceb7042279cd4b91e59dee6364a66fb2a23f8
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-11-07 12:02:20 +0800

    题目实现：单调栈

--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code26_1单调栈结构介绍.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code26_1单调栈结构介绍.java
@@ -1,7 +1,12 @@
 package com.c.mashibing.suanfa.tixike;
 
+import com.common.utils.LogarithmicUtil;
+
+import javax.print.attribute.IntegerSyntax;
+import java.util.ArrayList;
+import java.util.Stack;
+
 /*
-todo
  题目1，数组arr中没有重复值，实现一个单调栈
  题目2，数组arr中有重复值，实现一个单调栈
  */
@@ -15,7 +20,43 @@ public class Code26_1单调栈结构介绍 {
      * @return int[][]
      **/
     public static int[][] getNearLessNoRepeat(int[] arr){
-        return null;
+        if (arr == null || arr.length == 0){
+            return null;
+        }
+
+        int[][] res = new int[arr.length][2];
+        Stack<Integer> stack = new Stack<>();
+
+        //遍历数组，填充stack
+        for (int i = 0; i < arr.length; i++){
+            //stack栈顶的索引对应的数值是否大于arr[i]，满足条件就该弹出来并且结算了
+            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]){
+                int popIndex = stack.pop();
+                //左侧小于popIndex对应数值的数的索引
+                if (!stack.isEmpty()){
+                    res[popIndex][0] = stack.peek();
+                }else {
+                    res[popIndex][0] = -1;
+                }
+                //右侧小于popIndex对应数值的数的索引
+                res[popIndex][1] = i;
+            }
+            stack.add(i);
+        }
+
+        //把stack剩下的索引对应的数值都结算
+        while (!stack.isEmpty()){
+            int popIndex = stack.pop();
+            //左侧小于popIndex对应数值的数的索引
+            if (!stack.isEmpty()){
+                res[popIndex][0] = stack.peek();
+            }else {
+                res[popIndex][0] = -1;
+            }
+            //右侧小于popIndex对应数值的数的索引
+            res[popIndex][1] = -1;
+        }
+        return res;
     }
 
     /*
@@ -26,7 +67,67 @@ public class Code26_1单调栈结构介绍 {
      * @return int[][]
      **/
     public static int[][] getNearLess(int[] arr){
-        return null;
+        if (arr == null || arr.length == 0){
+            return null;
+        }
+
+        int[][] res = new int[arr.length][2];
+        Stack<ArrayList<Integer>> stack = new Stack<>();
+
+//        LogarithmicUtil.printArray(arr);
+//        System.out.println("进入for循环");
+        //遍历数组，填充stack
+        for (int i = 0; i < arr.length; i++){
+            //stack不为空
+            while (!stack.isEmpty()){
+                //stack栈顶的索引对应的数值是否大于arr[i]，满足条件就该弹出来并且结算了
+                if ( arr[stack.peek().get(0)] > arr[i]){
+                    ArrayList<Integer> popList = stack.pop();
+                    for (int popIndex : popList){
+                        //左侧小于popIndex对应数值的数的索引
+                        if (!stack.isEmpty()){
+                            //注意，结算的时候，拿的是list末尾的索引，才是最靠近索引i的索引值
+                            res[popIndex][0] = stack.peek().get(stack.peek().size() - 1);
+                        }else {
+                            res[popIndex][0] = -1;
+                        }
+                        //右侧小于popIndex对应数值的数的索引
+                        res[popIndex][1] = i;
+                    }
+                }
+                //stack栈顶的索引对应的数值是否小于arr[i]，满足条件就break，中止循环
+                else {
+                    break;
+                }
+            }
+
+            //stack栈顶的索引对应的数值是否等于arr[i]，满足条件就加到栈顶list里
+            if (!stack.isEmpty() && arr[stack.peek().get(0)] == arr[i]){
+                stack.peek().add(i);
+            }
+            //不等于，就正常new一个list加到栈顶
+            else {
+                ArrayList<Integer> newList = new ArrayList<>();
+                newList.add(i);
+                stack.add(newList);
+            }
+        }
+//        System.out.println("离开for循环");
+
+        while (!stack.isEmpty()){
+            ArrayList<Integer> popList = stack.pop();
+            for (int popIndex : popList){
+                //左侧小于popIndex对应数值的数的索引
+                if (!stack.isEmpty()){
+                    res[popIndex][0] = stack.peek().get(stack.peek().size() - 1);
+                }else {
+                    res[popIndex][0] = -1;
+                }
+                //右侧小于popIndex对应数值的数的索引
+                res[popIndex][1] = -1;
+            }
+        }
+        return res;
     }
 
     // for test
@@ -111,11 +212,12 @@ public class Code26_1单调栈结构介绍 {
         for (int i = 0; i < testTimes; i++) {
             int[] arr1 = getRandomArrayNoRepeat(size);
             int[] arr2 = getRandomArray(size, max);
-            if (!isEqual(getNearLessNoRepeat(arr1), rightWay(arr1))) {
-                System.out.println("Oops!");
-                printArray(arr1);
-                break;
-            }
+//            int[] arr2 = {1,11,1,5};
+//            if (!isEqual(getNearLessNoRepeat(arr1), rightWay(arr1))) {
+//                System.out.println("Oops!");
+//                printArray(arr1);
+//                break;
+//            }
             if (!isEqual(getNearLess(arr2), rightWay(arr2))) {
                 System.out.println("Oops!");
                 printArray(arr2);
