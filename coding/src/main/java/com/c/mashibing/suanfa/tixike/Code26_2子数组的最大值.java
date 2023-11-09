ff47f4372326d80a7fccc6f8d3e555cc58732a48
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-11-09 11:23:25 +0800

    题目实现：子数组的最大值、直方图最大长方形面积

--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code26_2子数组的最大值.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code26_2子数组的最大值.java
@@ -1,7 +1,11 @@
 package com.c.mashibing.suanfa.tixike;
 
+import com.common.utils.LogarithmicUtil;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
 /*
-todo
  题目1，
  给定一个只包含正数的数组arr，arr中任何一个子数组sub，
  一定都可以算出(sub累加和 )* (sub中的最小值)是什么，
@@ -19,12 +23,38 @@ public class Code26_2子数组的最大值 {
      * @return int
      **/
     public static int max2(int[] arr){
-        return 0;
+        if (arr == null || arr.length == 0){
+            return 0;
+        }
+
+        //累加和需要借助前缀和数组
+        int[] preSum = new int[arr.length];
+        preSum[0] = arr[0];
+        for (int i = 1; i < arr.length; i++){
+            preSum[i] = preSum[i-1] + arr[i];
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+        for (int i = 0; i < arr.length; i++){
+            //等于的时候，也要结算。所有等于的数的结算结果中，只有最后一个相等的数的结算结果是最大的，也只有这个数的结算结果是正确的。
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]){
+                int popIndex = stack.pop();
+                max = Math.max(max,arr[popIndex] * (preSum[i - 1] - (stack.isEmpty() ? 0 : preSum[stack.peek()])));
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()){
+            int popIndex = stack.pop();
+            max = Math.max(max,arr[popIndex] * (preSum[arr.length - 1] - (stack.isEmpty() ? 0 : preSum[stack.peek()])));
+        }
+        return max;
     }
 
 
     public static int[] gerenareRondomArray() {
-        int[] arr = new int[(int) (Math.random() * 20) + 10];
+        int[] arr = new int[(int) (Math.random() * 30) + 10];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (Math.random() * 101);
         }
@@ -36,8 +66,14 @@ public class Code26_2子数组的最大值 {
         System.out.println("test begin");
         for (int i = 0; i < testTimes; i++) {
             int[] arr = gerenareRondomArray();
-            if (max1(arr) != max2(arr)) {
+//            int[] arr = {7,5};
+            int res1 = max1(arr);
+            int res2 = max2(arr);
+            if (res1 != res2) {
                 System.out.println("FUCK!");
+                LogarithmicUtil.printArray(arr);
+                System.out.println(res1);
+                System.out.println(res2);
                 break;
             }
         }
--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code26_3直方图最大长方形面积.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code26_3直方图最大长方形面积.java
@@ -1,7 +1,8 @@
 package com.c.mashibing.suanfa.tixike;
 
+import java.util.Stack;
+
 /*
-todo
  题目1.
  给定一个非负数组arr，代表直方图
  返回直方图的最大长方形面积
@@ -17,8 +18,33 @@ public class Code26_3直方图最大长方形面积 {
      * @param height
      * @return int
      **/
-    public static int largestRectangleArea1(int[] height){
-        return 0;
+    public static int largestRectangleArea(int[] height){
+        if (height == null || height.length == 0){
+            return 0;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+        for (int i = 0; i < height.length; i++){
+            while (!stack.isEmpty() && height[stack.peek()] >= height[i]){
+                int popIndex = stack.pop();
+                //                    0->2          * ( (1 - 1) -  (-1))
+                max = Math.max(max,height[popIndex] * ((i - 1) - (stack.isEmpty() ? -1 : stack.peek())));
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()){
+            int popIndex = stack.pop();
+            max = Math.max(max,height[popIndex] * ((height.length - 1) - (stack.isEmpty() ? -1 : stack.peek())));
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {2,1,3,2};
+        System.out.println(largestRectangleArea(height));
+
     }
 
 }
