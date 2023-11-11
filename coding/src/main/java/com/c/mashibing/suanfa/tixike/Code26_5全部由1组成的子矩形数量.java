8d2b1264e4d860317727ca725ebc566fb20b99f8
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-11-11 15:07:33 +0800

    新题目：子矩形数量

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code26_5全部由1组成的子矩形数量.java
@@ -0,0 +1,23 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，
+ 给定一个二维数组matrix，其中的值不是0就是1，
+ 返回全部由1组成的子矩形数量
+ 测试链接：https://leetcode.com/problems/count-submatrices-with-all-ones
+ */
+public class Code26_5全部由1组成的子矩形数量 {
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/11/11 9:50
+     * @description 题目1
+     * @param mat
+     * @return int
+     **/
+    public static int numSubmat(int[][] mat){
+        return 0;
+    }
+}
