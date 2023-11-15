42d4eb4659e54de4d09b4bab3293220a55fd8e30
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-11-15 14:48:15 +0800

    新题目：斐波那契数列矩阵乘法方式的实现

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code27_2求斐波那契数列矩阵乘法的方法.java
@@ -0,0 +1,26 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，
+ 斐波那契数列矩阵乘法方式的实现
+ 1）斐波那契数列的线性求解（O(N)）的方式非常好理解
+ 2）同时利用线性代数，也可以改写出另一种表示
+ | F(N) , F(N-1) | = | F(2), F(1) |  *  某个二阶矩阵的N-2次方
+ 3）求出这个二阶矩阵，进而最快求出这个二阶矩阵的N-2次方
+
+ */
+public class Code27_2求斐波那契数列矩阵乘法的方法 {
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/11/15 14:47
+     * @description 题目1
+     * @param n
+     * @return int
+     **/
+    public static int f(int n){
+        return 0;
+    }
+}
