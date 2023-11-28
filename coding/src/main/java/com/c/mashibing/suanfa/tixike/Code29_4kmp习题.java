58ad44ab99c1d05a75e706c287abc1d8459d8b12
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-11-28 19:40:36 +0800

    新题目：判断是否为子树、判断是否为旋转字符串

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code29_4kmp习题.java
@@ -0,0 +1,57 @@
+package com.c.mashibing.suanfa.tixike;
+
+import java.util.HashMap;
+
+/*
+todo
+ 题目1，判断2个字符串是否是旋转字符串，如12345 45123是旋转字符串
+ 题目2，有2棵二叉树，判断其中一棵树是否是另一棵树的子树
+ 测试链接 : https://leetcode.cn/problems/subtree-of-another-tree/
+ */
+public class Code29_4kmp习题 {
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/11/28 10:54
+     * @description 题目1
+     * @param a
+     * @param b
+     * @return boolean
+     **/
+    public static boolean isRotation(String a, String b){
+        return false;
+    }
+
+
+
+
+
+    public static class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int v) {
+            val = v;
+        }
+    }
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/11/28 10:54
+     * @description 题目2
+     * @param big
+     * @param small
+     * @return boolean
+     **/
+    public static boolean isSubtree(TreeNode big, TreeNode small) {
+        return false;
+    }
+
+
+
+
+
+}
