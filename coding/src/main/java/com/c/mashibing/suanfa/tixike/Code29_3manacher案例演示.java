e1133fc9538c815684b9f354144888addf2256cd
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-11-29 14:13:48 +0800

    题目实现：manacher算法、判断是否为子树、判断是否为旋转字符串

--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code29_3manacher案例演示.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code29_3manacher案例演示.java
@@ -1,7 +1,6 @@
 package com.c.mashibing.suanfa.tixike;
 
 /*
-todo
  题目1，
  实现manacher算法，找到最长回文子串的大小
  */
@@ -16,6 +15,91 @@ public class Code29_3manacher案例演示 {
      * @return int
      **/
     public static int manacher(String s){
-        return 0;
+        if (s == null || s.length() == 0){
+            return 0;
+        }
+
+        //在s中每个字符左右都添加#字符
+        char[] str = addCharacter(s.toCharArray());
+
+        //记录以下标为i的字符串的最大回文半径
+        int[] pArr = new int[str.length];
+        int max = -1;
+        int C = -1, R = -1;
+        for (int i = 0; i < str.length; i++){
+            //i为中心，不用遍历，最小能扩到多少
+            pArr[i] = R > i ? Math.min(pArr[2 * C - i],R - i) : 1;
+            while ( i + pArr[i] < str.length && i - pArr[i] >= 0 && (str[i + pArr[i]] == str[i - pArr[i]])){
+                pArr[i]++;
+            }
+            if (i + pArr[i] > R){
+                R = i + pArr[i];
+                C = i;
+            }
+            max = Math.max(max,pArr[i]);
+        }
+        return max-1;
+    }
+
+    private static char[] addCharacter(char[] chars) {
+        char[] res = new char[chars.length * 2 + 1];
+        int index = 0;
+        for (int i = 0; i < res.length; i++){
+            res[i] = ((i & 1) == 1) ? chars[index++] : '#';
+        }
+        return res;
+    }
+
+    //*************************************************************************************
+    public static char[] manacherString(String str) {
+        char[] charArr = str.toCharArray();
+        char[] res = new char[str.length() * 2 + 1];
+        int index = 0;
+        for (int i = 0; i != res.length; i++) {
+            res[i] = (i & 1) == 0 ? '#' : charArr[index++];
+        }
+        return res;
+    }
+
+    // for test
+    public static int right(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        char[] str = manacherString(s);
+        int max = 0;
+        for (int i = 0; i < str.length; i++) {
+            int L = i - 1;
+            int R = i + 1;
+            while (L >= 0 && R < str.length && str[L] == str[R]) {
+                L--;
+                R++;
+            }
+            max = Math.max(max, R - L - 1);
+        }
+        return max / 2;
+    }
+
+    // for test
+    public static String getRandomString(int possibilities, int size) {
+        char[] ans = new char[(int) (Math.random() * size) + 1];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = (char) ((int) (Math.random() * possibilities) + 'a');
+        }
+        return String.valueOf(ans);
+    }
+
+    public static void main(String[] args) {
+        int possibilities = 5;
+        int strSize = 20;
+        int testTimes = 5000000;
+        System.out.println("test begin");
+        for (int i = 0; i < testTimes; i++) {
+            String str = getRandomString(possibilities, strSize);
+            if (manacher(str) != right(str)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("test finish");
     }
 }
--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code29_4kmp习题.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code29_4kmp习题.java
@@ -1,9 +1,12 @@
 package com.c.mashibing.suanfa.tixike;
 
+import org.springframework.util.CollectionUtils;
+
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 /*
-todo
  题目1，判断2个字符串是否是旋转字符串，如12345 45123是旋转字符串
  题目2，有2棵二叉树，判断其中一棵树是否是另一棵树的子树
  测试链接 : https://leetcode.cn/problems/subtree-of-another-tree/
@@ -20,12 +23,61 @@ public class Code29_4kmp习题 {
      * @return boolean
      **/
     public static boolean isRotation(String a, String b){
-        return false;
+        //不接受字符串为空，或者2个字符串长度不相等
+        if (a == null || b == null || a.length() == 0 || b.length() == 0 || a.length() != b.length()){
+            return false;
+        }
+
+        String a2 = a + a;
+        return kmp(a2,b) != -1;
     }
 
+    //kmp算法，用于判断s2是否为s1的子串
+    private static int kmp(String s1, String s2) {
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+        //获取b的next信息
+        int[] next = getNext(c2);
+        int x = 0, y = 0;
 
+        while (x < c1.length && y < c2.length){
+            if (c1[x] == c2[y]){
+                x++;
+                y++;
+            }
+            else if (y == 0){
+                x++;
+            }else {
+                y = next[y];
+            }
+        }
+        return y >= c2.length ? x - c2.length : -1;
+    }
 
+    //获取每个元素之前字符数组中，前子数组和后子数组最长有多少相等，返回长度
+    private static int[] getNext(char[] chars) {
+        int[] next = new int[chars.length];
+        next[0] = -1;
+        next[1] = 0;
 
+        int cur = 2;
+        int pre = 0;
+
+        while (cur < next.length){
+            if (chars[cur-1] == chars[pre]){
+                next[cur++] = ++pre;
+            }
+            else if (pre == 0){
+                next[cur++] = 0;
+            }else {
+                pre = next[pre];
+            }
+        }
+
+        return next;
+    }
+
+    //*************************************************************************
 
     public static class TreeNode {
         public int val;
@@ -47,11 +99,133 @@ public class Code29_4kmp习题 {
      * @return boolean
      **/
     public static boolean isSubtree(TreeNode big, TreeNode small) {
-        return false;
+        if (big == null || small == null){
+            return false;
+        }
+
+        List<String> bigList = new LinkedList<>();
+        List<String> smallList = new LinkedList<>();
+        preSerialize(big,bigList);
+        preSerialize(small,smallList);
+        String[] bigArr = toArray(bigList);
+        String[] smallArr = toArray(smallList);
+        return kmp2(bigArr,smallArr) != -1;
+    }
+
+    private static int kmp2(String[] c1, String[] c2) {
+        //获取b的next信息
+        int[] next = getNext2(c2);
+        int x = 0, y = 0;
+
+        while (x < c1.length && y < c2.length){
+//            System.out.print("c1[x]: " + c1[x] + " c2[y]: " + c2[y] + " " );
+//            System.out.println("判断结果: " + c1[x] == c2[y]);
+//            if ("4".equals(c1[x]) && "4".equals(c2[y])){
+//                System.out.println("c1[x] identityHashCode: " + System.identityHashCode(c1[x]));
+//                System.out.println("c2[y] identityHashCode: " + System.identityHashCode(c2[y]));
+//            }
+            if (c1[x].equals(c2[y])){
+                x++;
+                y++;
+            }
+            else if (y == 0){
+                x++;
+            }else {
+                y = next[y];
+            }
+        }
+        return y >= c2.length ? x - c2.length : -1;
+    }
+
+    //获取每个元素之前字符数组中，前子数组和后子数组最长有多少相等，返回长度
+    private static int[] getNext2(String[] strs) {
+        int[] next = new int[strs.length];
+        next[0] = -1;
+        next[1] = 0;
+
+        int cur = 2;
+        int pre = 0;
+
+        while (cur < next.length){
+            if (strs[cur-1].equals(strs[pre])){
+                next[cur++] = ++pre;
+            }
+            else if (pre == 0){
+                next[cur++] = 0;
+            }else {
+                pre = next[pre];
+            }
+        }
+
+        return next;
+    }
+
+    private static String[] toArray(List<String> list) {
+        String[] res = new String[list.size()];
+        for (int i = 0; i < res.length; i++){
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+
+    private static void preSerialize(TreeNode root,List<String> list) {
+        if (root == null){
+            list.add("null");
+            return;
+        }
+        list.add(String.valueOf(root.val));
+        preSerialize(root.left,list);
+        preSerialize(root.right,list);
     }
 
 
+//    public static void main(String[] args) {
+//        String str1 = "yunzuocheng";
+//        String str2 = "zuochengyun";
+//        System.out.println(isRotation(str1, str2));
+//
+//    }
+
+    public static void main(String[] args) {
+//        TreeNode node1 = new TreeNode(1);
+//        TreeNode node2 = new TreeNode(2);
+//        TreeNode node3= new TreeNode(3);
+//        TreeNode node4 = new TreeNode(4);
+//        TreeNode node5 = new TreeNode(5);
+//        TreeNode node0 = new TreeNode(0);
+//        node3.left = node4;
+//        node3.right = node5;
+//        node4.left = node1;
+//        node4.right = node2;
+//        node2.left = node0;
+//
+//
+//        TreeNode node6 = new TreeNode(4);
+//        TreeNode node7 = new TreeNode(1);
+//        TreeNode node8 = new TreeNode(2);
+//        node6.left = node7;
+//        node6.right = node8;
+//
+//        System.out.println(isSubtree(node3,node6));
+
+        TreeNode node1= new TreeNode(1);
+        TreeNode node2= new TreeNode(2);
+        TreeNode node3= new TreeNode(3);
+        TreeNode node4= new TreeNode(4);
+        TreeNode node5= new TreeNode(5);
+        node3.left = node4;
+        node3.right = node5;
+        node4.left = node1;
+        node4.right = node2;
 
 
+        TreeNode node6= new TreeNode(4);
+        TreeNode node7= new TreeNode(1);
+        TreeNode node8= new TreeNode(2);
+        node6.left = node7;
+        node6.right = node8;
 
+        System.out.println(isSubtree(node3,node6));
+
+    }
 }
