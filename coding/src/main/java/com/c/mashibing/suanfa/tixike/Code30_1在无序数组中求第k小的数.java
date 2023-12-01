7ca9f634f1d31eb2c8733bfe7de7e1f56fdb15aa
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-12-01 15:45:47 +0800

    新题目：在无序数组中求第k小的数、bfprt算法

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code30_1在无序数组中求第k小的数.java
@@ -0,0 +1,27 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，在无序数组中求第k小的数。
+ 使用改写快排，时间复杂度o(N)
+ 题目2，同题目1，
+ 使用bfprt算法，时间复杂度为o(N)
+ */
+public class Code30_1在无序数组中求第k小的数 {
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/12/1 11:01
+     * @description 题目1
+     * @param array
+     * @param k
+     * @return int
+     **/
+    public static int minKth1(int[] array, int k){
+        return 0;
+    }
+
+
+
+}
---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code30_2bfprt算法.java
@@ -0,0 +1,22 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，在无序数组中求第k小的数。
+ 使用bfprt算法，时间复杂度o(N)
+ */
+public class Code30_2bfprt算法 {
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/12/1 11:03
+     * @description 题目1
+     * @param array
+     * @param k
+     * @return int
+     **/
+    public static int minKth1(int[] array, int k){
+        return 0;
+    }
+}
