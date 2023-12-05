aed3a25cd0b54a53a56deaeca7372deef4c2944f
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-12-05 14:08:18 +0800

    题目实现：蓄水池算法

--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code30_4蓄水池算法.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code30_4蓄水池算法.java
@@ -9,4 +9,32 @@ todo
 
  */
 public class Code30_4蓄水池算法 {
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/12/4 21:03
+     * @description 题目1
+     * @param ballNum
+     * @param bag
+     **/
+    public static void qs1_process(int ballNum,int[] bag){
+        int index = 0;
+        for(int i = 1; i <= ballNum; i++){
+            if (i <= 10){
+                bag[index++] = i;
+            }else {
+                //每个数都有10/i的几率进入bag
+                if (randNum(i) <= 10){
+                    index = (int)(Math.random() * 10);
+                    bag[index] = i;
+                }
+            }
+        }
+    }
+
+    //产生范围为[1, i]的随机整数
+    private static int randNum(int i) {
+        return (int)(Math.random() * i) + 1;
+    }
 }
