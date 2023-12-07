7b054828757d853a8d2e356675206fb911541497
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-12-07 11:46:11 +0800

    新题目：morris遍历，以及基于morris遍历的前序中序后续遍历、基于morris遍历的判断是否为搜索二叉树

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code31_2课堂答疑复杂度.java
@@ -0,0 +1,56 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，实现morris遍历.
+ 将额外空间复杂度从logN降低为1
+ 题目2，基于morris遍历的前序
+ 题目3，基于morris遍历的中序
+ */
+public class Code31_2课堂答疑复杂度 {
+
+
+    public static class Node {
+        public int value;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/12/7 10:15
+     * @description 题目1，morris遍历
+     * @param head
+     **/
+    public static void morris(Node head){
+
+
+    }
+
+    /*
+     * @author gengzhihao
+     * @date 2023/12/7 10:15
+     * @description 题目2
+     * @param head
+     **/
+    public static void morrisPre(Node head){
+
+
+    }
+
+    /*
+     * @author gengzhihao
+     * @date 2023/12/7 10:15
+     * @description 题目3
+     * @param head
+     **/
+    public static void morrisIn(Node head){
+
+
+    }
+}
---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code31_3习题实战演示.java
@@ -0,0 +1,43 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，基于morris遍历实现后序遍历
+ 题目2，基于morris遍历判断是否为搜索二叉树
+ */
+public class Code31_3习题实战演示 {
+
+
+    public static class Node {
+        public int value;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/12/7 11:44
+     * @description 题目1
+     * @param head
+     **/
+    public static void morrisPos(Node head){
+
+
+    }
+
+    /*
+     * @author gengzhihao
+     * @date 2023/12/7 11:44
+     * @description 题目2
+     * @param head
+     * @return boolean
+     **/
+    public static boolean isBST(Node head){
+        return false;
+    }
+}
