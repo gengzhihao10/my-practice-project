1555eb0232ca9e94c911ed65472d1f2428706386
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-12-08 10:33:15 +0800

    题目实现：morris遍历，以及基于morris遍历的前序中序后续遍历、基于morris遍历的判断是否为搜索二叉树

--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code31_2课堂答疑复杂度.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code31_2课堂答疑复杂度.java
@@ -1,7 +1,6 @@
 package com.c.mashibing.suanfa.tixike;
 
 /*
-todo
  题目1，实现morris遍历.
  将额外空间复杂度从logN降低为1
  题目2，基于morris遍历的前序
@@ -28,7 +27,34 @@ public class Code31_2课堂答疑复杂度 {
      * @param head
      **/
     public static void morris(Node head){
+        if (head == null){
+            return;
+        }
 
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null){
+            mostRight = cur.left;
+            //cur有左孩子
+            if (mostRight != null){
+                //找到cur左子树的最右孩子
+                while (mostRight.right != null && mostRight.right != cur){
+                    mostRight = mostRight.right;
+                }
+                //第一次来到mostRight，此时right指针还没有被修改
+                if (mostRight.right == null){
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                }
+                //第二次来到mostRight，此时right指针已经被修改到指向cur
+                else{
+                    mostRight.right = null;
+                }
+            }
+            //cur向右移动
+            cur = cur.right;
+        }
 
     }
 
@@ -39,8 +65,39 @@ public class Code31_2课堂答疑复杂度 {
      * @param head
      **/
     public static void morrisPre(Node head){
+        if (head == null){
+            return;
+        }
 
-
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null){
+            mostRight = cur.left;
+            //cur有左孩子
+            if (mostRight != null){
+                //找到cur左子树的最右孩子
+                while (mostRight.right != null && mostRight.right != cur){
+                    mostRight = mostRight.right;
+                }
+                //第一次来到mostRight，此时right指针还没有被修改
+                if (mostRight.right == null){
+                    System.out.print(cur.value + " ");
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                }
+                //第二次来到mostRight，此时right指针已经被修改到指向cur
+                else{
+                    mostRight.right = null;
+                }
+            }
+            else {
+                System.out.print(cur.value + " ");
+            }
+            //cur向右移动
+            cur = cur.right;
+        }
+        System.out.println();
     }
 
     /*
@@ -50,7 +107,89 @@ public class Code31_2课堂答疑复杂度 {
      * @param head
      **/
     public static void morrisIn(Node head){
+        if (head == null){
+            return;
+        }
+
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null){
+            mostRight = cur.left;
+            //cur有左孩子
+            if (mostRight != null){
+                //找到cur左子树的最右孩子
+                while (mostRight.right != null && mostRight.right != cur){
+                    mostRight = mostRight.right;
+                }
+                //第一次来到mostRight，此时right指针还没有被修改
+                if (mostRight.right == null){
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                }
+                //第二次来到mostRight，此时right指针已经被修改到指向cur
+                else{
+                    mostRight.right = null;
+                }
+            }
+            System.out.print(cur.value + " ");
+            //cur向右移动
+            cur = cur.right;
+        }
+        System.out.println();
+
+    }
 
 
+    //*****************************************************************************************
+
+    public static void main(String[] args) {
+        Node head = new Node(4);
+        head.left = new Node(2);
+        head.right = new Node(6);
+        head.left.left = new Node(1);
+        head.left.right = new Node(3);
+        head.right.left = new Node(5);
+        head.right.right = new Node(7);
+        printTree(head);
+        System.out.println("中序遍历：");
+        morrisIn(head);
+        System.out.println("前序遍历：");
+        morrisPre(head);
+//        morrisPos(head);
+        printTree(head);
+
+    }
+
+    // for test -- print tree
+    public static void printTree(Node head) {
+        System.out.println("Binary Tree:");
+        printInOrder(head, 0, "H", 17);
+        System.out.println();
+    }
+
+
+    public static void printInOrder(Node head, int height, String to, int len) {
+        if (head == null) {
+            return;
+        }
+        printInOrder(head.right, height + 1, "v", len);
+        String val = to + head.value + to;
+        int lenM = val.length();
+        int lenL = (len - lenM) / 2;
+        int lenR = len - lenM - lenL;
+        val = getSpace(lenL) + val + getSpace(lenR);
+        System.out.println(getSpace(height * len) + val);
+        printInOrder(head.left, height + 1, "^", len);
+    }
+
+
+    public static String getSpace(int num) {
+        String space = " ";
+        StringBuffer buf = new StringBuffer("");
+        for (int i = 0; i < num; i++) {
+            buf.append(space);
+        }
+        return buf.toString();
     }
 }
--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code31_3习题实战演示.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code31_3习题实战演示.java
@@ -1,7 +1,6 @@
 package com.c.mashibing.suanfa.tixike;
 
 /*
-todo
  题目1，基于morris遍历实现后序遍历
  题目2，基于morris遍历判断是否为搜索二叉树
  */
@@ -26,8 +25,59 @@ public class Code31_3习题实战演示 {
      * @param head
      **/
     public static void morrisPos(Node head){
+        if (head == null){
+            return;
+        }
 
+        Node cur = head;
+        Node mostRight = null;
 
+        while (cur != null){
+            mostRight = cur.left;
+            if (mostRight != null){
+                while (mostRight.right != null && mostRight.right != cur){
+                    mostRight = mostRight.right;
+                }
+
+                if (mostRight.right == null){
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                }
+                else {
+                    mostRight.right = null;
+                    //逆序打印左子树的右边界
+                    printRightEdgeByReversed(cur.left);
+                }
+            }
+            cur = cur.right;
+        }
+        printRightEdgeByReversed(cur);
+    }
+
+    //逆序打印root节点的右边界
+    private static void printRightEdgeByReversed(Node root) {
+        Node newHead = reverseRightEdge(root);
+        Node cur = newHead;
+        while (cur != null){
+            System.out.print(cur.value + " ");
+            cur = cur.right;
+        }
+        reverseRightEdge(newHead);
+    }
+
+    //反转
+    private static Node reverseRightEdge(Node root) {
+        Node pre = null;
+        Node cur = root;
+        Node next = null;
+        while (cur != null){
+            next = cur.right;
+            cur.right = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
     }
 
     /*
@@ -38,6 +88,37 @@ public class Code31_3习题实战演示 {
      * @return boolean
      **/
     public static boolean isBST(Node head){
-        return false;
+        if (head == null){
+            return false;
+        }
+
+        Node cur = head;
+        Node mostRight = null;
+        Node pre = null;
+        boolean ans = true;
+
+        while (cur != null){
+            mostRight = cur.left;
+            if (mostRight != null){
+                while (mostRight.right != null && mostRight.right != cur){
+                    mostRight = mostRight.right;
+                }
+
+                if (mostRight.right == null){
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                }
+                else {
+                    mostRight.right = null;
+                }
+            }
+            if (pre != null && pre.value >= cur.value){
+                ans =false;
+            }
+            pre = cur;
+            cur = cur.right;
+        }
+        return ans;
     }
 }
