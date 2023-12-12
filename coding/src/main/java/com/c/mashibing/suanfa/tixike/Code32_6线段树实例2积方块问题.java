b58727b7c559f62ce6f3bde880345b04da9c0ab9
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-12-12 16:04:13 +0800

    新题目：线段树的实现、积方块问题

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code32_6线段树实例2积方块问题.java
@@ -0,0 +1,32 @@
+package com.c.mashibing.suanfa.tixike;
+
+import java.util.List;
+
+/*
+todo
+ 题目1，
+ 想象一下标准的俄罗斯方块游戏，X轴是积木最终下落到底的轴线
+ 下面是这个游戏的简化版：
+ 1）只会下落正方形积木
+ 2）[a,b] -> 代表一个边长为b的正方形积木，积木左边缘沿着X = a这条线从上方掉落
+ 3）认为整个X轴都可能接住积木，也就是说简化版游戏是没有整体的左右边界的
+ 4）没有整体的左右边界，所以简化版游戏不会消除积木，因为不会有哪一层被填满。
+ 给定一个N*2的二维数组matrix，可以代表N个积木依次掉落，
+ 返回每一次掉落之后的最大高度
+ https://leetcode.com/problems/falling-squares/
+
+ */
+public class Code32_6线段树实例2积方块问题 {
+
+
+    /*
+     * @author gengzhihao
+     * @date 2023/12/12 16:00
+     * @description 题目1
+     * @param positions
+     * @return List<Integer>
+     **/
+    public List<Integer> fallingSquares(int[][] positions){
+        return null;
+    }
+}
