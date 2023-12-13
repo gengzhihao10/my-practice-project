cff910550a91f7144f6ef3c93ec749adade463ef
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-12-13 14:15:33 +0800

    题目实现：线段树的实现

--- coding/src/main/java/com/c/mashibing/suanfa/tixike/Code32_3线段树code实现数组arr时间复杂度都是LogN.java
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code32_3线段树code实现数组arr时间复杂度都是LogN.java
@@ -1,7 +1,6 @@
 package com.c.mashibing.suanfa.tixike;
 
 /*
-todo
  题目1，给定一个数组arr，用户希望你实现如下三个方法
  1）void add(int L, int R, int V) :  让数组arr[L…R]上每个数都加上V
  2）void update(int L, int R, int V) :  让数组arr[L…R]上每个数都变成V
@@ -21,29 +20,124 @@ public class Code32_3线段树code实现数组arr时间复杂度都是LogN {
         private int MAXN;
         private int[] arr;
         private int[] sum;
-        private int[] lazy;
-        private int[] change;
-        private boolean[] update;
+        private int[] add;
+        private int[] update;
+        private boolean[] updateOrNot;
 
 
         public SegmentTree(int[] origin){
-
+            MAXN = origin.length + 1;
+            arr = new int[MAXN];
+            for (int i = 0; i < origin.length; i++){
+                arr[i + 1] = origin[i];
+            }
+            sum = new int[MAXN << 2];
+            add = new int[MAXN << 2];
+            update = new int[MAXN << 2];
+            updateOrNot = new boolean[MAXN << 2];
         }
 
-        public void build(int l, int r, int rt){
-
+        public void build(int l, int r, int root){
+            if (l == r){
+                sum[root] = arr[l];
+                return;
+            }
+            int mid = (l + r) / 2;
+            build(l,mid,root << 1);
+            build(mid + 1,r,root << 1 | 1);
+            sum[root] = sum[root << 1] + sum[root << 1 | 1];
         }
 
-        public void add(int L, int R, int C, int l, int r, int rt){
 
+        //将lazy信息向下推
+        private void pushDown(int root, int ln, int rn) {
+
+            //如果有root的update信息，update的lazy信息向下推
+            if (updateOrNot[root]){
+                sum[root << 1] = update[root] * ln;
+                add[root << 1] = 0;
+                updateOrNot[root << 1] = true;
+                update[root << 1] = update[root];
+
+                sum[root << 1 | 1] = update[root] * rn;
+                add[root << 1 | 1] = 0;
+                updateOrNot[root << 1 | 1] = true;
+                update[root << 1 | 1] = update[root];
+
+                updateOrNot[root] = false;
+            }
+            //如果有root的add信息，add的lazy信息向下推
+            if (add[root] != 0){
+                sum[root << 1] += add[root] * ln;
+                add[root << 1] += add[root];
+
+                sum[root << 1 | 1] +=  add[root] * rn;
+                add[root << 1 | 1] +=  add[root];
+
+                add[root] = 0;
+
+            }
         }
 
-        public void update(int L, int R, int C, int l, int r, int rt){
 
+        public void add(int L, int R, int C, int l, int r, int root){
+            if (L <= l && R >= r){
+                sum[root] = sum[root] + C * (r - l + 1);
+                add[root] = add[root] + C;
+                return;
+            }
+
+            int mid = (l + r) / 2;
+
+            pushDown(root,mid - l + 1,r - mid);
+
+            if (L <= mid){
+                add(L,R,C,l,mid,root << 1);
+            }
+            if (mid + 1 <= R){
+                add(L,R,C,mid +1,r,root << 1 | 1);
+            }
+            sum[root] = sum[root << 1] + sum[root << 1 | 1];
         }
 
-        public long query(int L, int R, int l, int r, int rt){
-            return 0L;
+
+        public void update(int L, int R, int C, int l, int r, int root){
+            if (L <= l && R >= r){
+                sum[root] = C * (r - l + 1);
+                updateOrNot[root] = true;
+                update[root] = C;
+                add[root] = 0;
+                return;
+            }
+
+            int mid = (l + r) / 2;
+            pushDown(root,mid - l + 1,r - mid);
+
+            if (L <= mid){
+                update(L,R,C,l,mid,root << 1);
+            }
+            if (mid + 1 <= R){
+                update(L,R,C,mid +1,r,root << 1 | 1);
+            }
+            sum[root] = sum[root << 1] + sum[root << 1 | 1];
+        }
+
+        public long query(int L, int R, int l, int r, int root){
+            if (L <= l && R >= r){
+                return sum[root];
+            }
+
+            int mid = (l + r) / 2;
+            pushDown(root,mid - l + 1,r - mid);
+
+            long ans = 0L;
+            if (L <= mid){
+                ans += query(L,R,l,mid,root << 1);
+            }
+            if (mid + 1 <= R){
+                ans += query(L,R,mid +1,r,root << 1 | 1);
+            }
+            return ans;
         }
     }
 
