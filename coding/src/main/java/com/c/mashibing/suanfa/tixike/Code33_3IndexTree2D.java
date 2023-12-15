46180e52e599850fb3e94a3cc631a56854661410
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2023-12-15 15:45:47 +0800

    新题目：IndexTree、二阶IndexTree

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code33_3IndexTree2D.java
@@ -0,0 +1,27 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，IndexTree改成二维
+ 测试链接：https://leetcode.com/problems/range-sum-query-2d-mutable
+ 但这个题是付费题目
+ 提交时把类名、构造函数名从Code02_IndexTree2D改成NumMatrix
+ */
+public class Code33_3IndexTree2D {
+
+    public class Code02_IndexTree2D{
+        public Code02_IndexTree2D(int[][] matrix){
+
+        }
+
+        public void update(int row, int col, int val){
+
+
+        }
+
+
+        public int sumRegion(int row1, int col1, int row2, int col2){
+            return 0;
+        }
+    }
+}
