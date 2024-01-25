22673285eec00e611f2c4f88a8a80f9f89b171e6
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2024-01-25 11:34:53 +0800

    新题目：根据对数器猜规律三道题

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code39_1买苹果返回袋子数.java
@@ -0,0 +1,37 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，
+ 小虎去买苹果，商店只提供两种类型的塑料袋，每种类型都有任意数量。
+ 1）能装下6个苹果的袋子
+ 2）能装下8个苹果的袋子
+ 小虎可以自由使用两种袋子来装苹果，但是小虎有强迫症，他要求自己使用的袋子数量必须最少，且使用的每个袋子必须装满。
+ 给定一个正整数N，返回至少使用多少袋子。如果N无法让使用的每个袋子必须装满，返回-1
+
+ */
+public class Code39_1买苹果返回袋子数 {
+
+
+    /*
+     * @author gengzhihao
+     * @date 2024/1/25 9:15
+     * @description 暴力解
+     * @param apple
+     * @return int
+     **/
+    public static int minBags0(int apple){
+        return 0;
+    }
+
+    /*
+     * @author gengzhihao
+     * @date 2024/1/25 9:06
+     * @description 题目1
+     * @param apple
+     * @return int
+     **/
+    public static int minBags(int apple){
+        return 0;
+    }
+}
