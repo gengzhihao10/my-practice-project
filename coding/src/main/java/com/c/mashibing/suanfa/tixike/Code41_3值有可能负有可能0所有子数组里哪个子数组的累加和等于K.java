24b65e385fe838ac1baaebb1a8136612f4462981
不想睡觉的橘子君 <68325723+gengzhihao10@users.noreply.github.com>
2024-02-02 11:51:01 +0800

    新题目：正整数无序数组arr累加和等于K、整数无序数组arr累加和等于K

---
+++ coding/src/main/java/com/c/mashibing/suanfa/tixike/Code41_3值有可能负有可能0所有子数组里哪个子数组的累加和等于K.java
@@ -0,0 +1,91 @@
+package com.c.mashibing.suanfa.tixike;
+
+/*
+todo
+ 题目1，
+ 给定一个整数组成的无序数组arr，值可能正、可能负、可能0
+ 给定一个整数值K
+ 找到arr的所有子数组里，哪个子数组的累加和等于K，并且是长度最大的
+ 返回其长度
+
+ */
+public class Code41_3值有可能负有可能0所有子数组里哪个子数组的累加和等于K {
+
+
+    /*
+     * @author gengzhihao
+     * @date 2024/2/2 11:21
+     * @description 题目1
+     * @param arr
+     * @param k
+     * @return int
+     **/
+    public static int maxLength(int[] arr, int k){
+        return 0;
+    }
+
+
+
+    // for test
+    public static int right(int[] arr, int K) {
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (valid(arr, i, j, K)) {
+                    max = Math.max(max, j - i + 1);
+                }
+            }
+        }
+        return max;
+    }
+
+    // for test
+    public static boolean valid(int[] arr, int L, int R, int K) {
+        int sum = 0;
+        for (int i = L; i <= R; i++) {
+            sum += arr[i];
+        }
+        return sum == K;
+    }
+
+    // for test
+    public static int[] generateRandomArray(int size, int value) {
+        int[] ans = new int[(int) (Math.random() * size) + 1];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = (int) (Math.random() * value) - (int) (Math.random() * value);
+        }
+        return ans;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        for (int i = 0; i != arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int len = 50;
+        int value = 100;
+        int testTime = 500000;
+
+        System.out.println("test begin");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generateRandomArray(len, value);
+            int K = (int) (Math.random() * value) - (int) (Math.random() * value);
+            int ans1 = maxLength(arr, K);
+            int ans2 = right(arr, K);
+            if (ans1 != ans2) {
+                System.out.println("Oops!");
+                printArray(arr);
+                System.out.println("K : " + K);
+                System.out.println(ans1);
+                System.out.println(ans2);
+                break;
+            }
+        }
+        System.out.println("test end");
+
+    }
+}
